9b0c43f7751beaaf5fdaa6eb9f7835b4818cf612
zacanger <zac@zacanger.com>
2015-10-16 16:48:20 -0600

    getting there...

--- closures.js
+++ closures.js
@@ -6,19 +6,14 @@ var outer = function(){
     return 'The original name was ' + name;
   }
 }
+
 //Above you're given a function that returns another function which has a closure over the name variable.
 //Invoke outer saving the return value into another variable called 'inner'.
 
-  //Code Here
-
+outer(arg)
 //Once you do that, invoke inner.
 
   //Code Here
-
-
-
-//Next problem
-
 
 
 var callFriend = function(){
@@ -32,19 +27,17 @@ var callFriend = function(){
 //Above you're given a callFriend function that returns another function.
 //Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
 
-  //Code Here
+callFriend('Jake', '435-215-9248');
 
+/* Write a function called makeCounter that makes the following code work properly. */
 
-
-//Next Problem
-
-
-
-/*
-  Write a function called makeCounter that makes the following code work properly.
-*/
-
-  //Code Here
+function makeCounter() {
+  var counter = 0; //local scoped makeCounter and children
+    return function myCounter() {
+      counter++;
+    }
+}
+  
   var count = makeCounter();
   count() // 1
   count() // 2
@@ -52,30 +45,25 @@ var callFriend = function(){
   count() // 4
 
 
-
-//Next Problem
-
-
-
 /*
   Write a function that does something simple (console, alert, etc). Write a second function that accepts the first function as it's first parameter. The second function should return a new third function which, when invoked, invokes the first, original function that was passed in, but will only ever do so once.
 */
 
-  //Code Here
+function foo() {
+  console.log('baz');
+}
 
+var arg = foo();
 
-
-//Next Problem
+function bar(arg) {
+  return function(bar());
+}
 
 
 
 /*
   Now, similar to the last problem, write a function called 'fnCounter' that accepts two parameters. The first parameter will be an anonymous function and the second parameter, 'N', will be a number. Now, in 'fnCounter', allow the anonymous funciton to be invoked 'N' number of times. After it's been invoked 'N' number of times, return 'STOP'.
 */
-
-
-
-//Next Problem
 
 
 
@@ -88,27 +76,26 @@ var callFriend = function(){
     }
   };
 
-  Above you have a function named counter. Examine the function (without running the code) then below write what you expect to happen when the funciton is invoked. *Hint: setTimeout calls a function or evaluates an expression after a specified number of milliseconds.
+  Above you have a function named counter. Examine the function (without running the code) then below write what you expect to happen when the funciton is invoked. *Hint: setTimeout calls a function or evaluates an expression after a specified number of milliseconds. */
 
-    //Answer Here
+alert("This will log '6' to the console, five times, one second apart.");
 
+/*  Now, run the function in your console and note what happpens.
+  Was your answer right or wrong? */
 
-  Now, run the function in your console and note what happpens.
+alert("If I was wrong, I'd never hear the end of it, I'm sure.");
 
-  Was your answer right or wrong?
+/* Fix the counter function so that it works the way you expect it to work. (logging 1 then 2 then 3, etc) */
 
-    //Answer Here
+console.log("Since that is how I expected it to work, do I still have to fix it?");
 
+/////
 
-  Fix the counter function so that it works the way you expect it to work. (logging 1 then 2 then 3, etc)
-*/
-
-    //Code Here
-
-
-
-//Next Problem
-
+var counter = function() {
+  for (var i = 1; i <= 5; i++) {
+    setTimeout(function timer() {
+      console.log(i); }
+    i*1000) }; };
 
 
 /*
@@ -124,4 +111,5 @@ var callFriend = function(){
   *Hint: Don't let this fool you. Break down what's really happening here.
 */
 
+console.log("I'm not a mind-reader. I hiave no idea what you're trying to accomplish, here. A function that loops through an array and returns... indices?");
 
--- fns-and-scope.js
+++ fns-and-scope.js
@@ -1,80 +1,79 @@
 //Once you complete a problem, open up Chrome and check the answer in the console.
 
-
 var name = 'Tyler';
 //Create a function called isTyler that accepts name as it's only argument.
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
-  //Code Here
-
-
-//Next problem
-
-
+function isTyler(name) {
+  if (name === 'Tyler') {
+    return true; }
+  else {
+    return false; }
+}
 
 //Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
 
-
-  //Code Here
-
-
-//Next Problem
-
+function getName() {
+  var name = prompt("What's your name?");
+  return ("Howdy, " + name + "!");
+}
 
 
 //Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
 //then alerts "Welcome, " plus whatever the users name is.
 
-  //Code Here
-
-
-//Next problem
-
-
+// oh... uh, see my answer to the previous problem?
 
 
 //What is the difference between arguments and parameters?
 
-  //Answer Here
-
-
-//Next problem
-
+// parameters: could be thought of sort of as the placeholders or variables that get passed to something;
+// arguments: the 'real' values that you would actually pass.
 
 
 //What are all the falsy values in JavaScript and how do you check if something is falsy?
 
-
-  //Answer Here
-
-
-
-//Next Problem
-
+// Nan, null, false, undefined, 0, "".
+console.log(!!(null));
+// or
+function isThisFalse(arg) {
+if (arg === false) {
+  return false; }
+else {
+  return ('Nope, not false, sorry.'); }
+}
 
 
 //Create a function called myName that returns your name
 
-  //Code Here
+function myName() {
+  var z = 'Zac Anger';
+  return z;
+}
 
 //Now save the function definition of myName into a new variable called newMyName
 
-  //Code Here
+var newMyName = myName();
+
 
 //Now alert the result of invoking newMyName
 
-
-
-//Next problem
-
+alert(newMyName);
 
 
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
-  //Code Here
-
+function outerFn() {
+  return function() {
+    return newMyName;
+  }
+}
+  
 //Now save the result of invoking outerFn into a variable called innerFn.
 
-  //Code Here
+var innerFn = outerFn();
 
 //Now invoke innerFn.
+
+console.log(innerFn);
+
