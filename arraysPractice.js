2aa76419cc0e7b285238695ca9b8a4a608bcb900
zacanger <zac@zacanger.com>
2015-10-19 16:05:47 -0600

    modified:   arraysPractice.js

--- arraysPractice.js
+++ arraysPractice.js
@@ -87,39 +87,40 @@ function reverse(str) {
 	return reversed;
 }
 
+
 var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
-/*
-  Here we're going to write a function that mimics going shopping and checking things off of our grocery list,
-  and adding new items to our list. 
-  
-  Write a function called removeItem that is given two arguments, the first is myGroceryList, and the
-  second is an item to remove from myGroceryList. If the second argument (or the item to add or remove) matches an item in myGroceryList,
-  remove that item from the your grocery list and return the new, updated grocery list. 
-  
-  Once you do that, write another function called addItem that is given two arguments, the first is myGroceryList
-  and the second is an item to add to your grocery list. In addItem add the item you passed in to
-  myGroceryList then return the new, updated grocery list.
-*/
+/*  Here we're going to write a function that mimics going shopping and checking things off of our grocery list,
+  and adding new items to our list.
+Write a function called removeItem that is given two arguments, the first is myGroceryList, and the second is an item to remove from myGroceryList. If the second argument (or the item to add or remove) matches an item in myGroceryList, remove that item from the your grocery list and return the new, updated grocery list.
+Once you do that, write another function called addItem that is given two arguments, the first is myGroceryList and the second is an item to add to your grocery list. In addItem add the item you passed in to myGroceryList then return the new, updated grocery list. */
 
-  //Code Here
+function removeItem(myGroceryList, item) {
+	if (myGroceryList[x] == item) {
+		myGroceryList.splice[item];
+	}
+	return myGroceryList;
+}
 
-//removeItem(myGroceryList, 'chips') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs'];
-//addItem(myGroceryList, 'Jerky') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs', 'Jerky'];
+function addItem(myGroceryList, item) {
+	myGroceryList.push[item];
+	return myGroceryList;
+}
 
-
-
-//Next Problem
-
+removeItem(myGroceryList, 'chips') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs'];
+addItem(myGroceryList, 'Jerky') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs', 'Jerky'];
 
 
 //Write a function called maker that creates an array, fills that array with numbers from 1 to 215, then returns the array.
 
-  //Code Here
+function maker() {
+  var filled = [];
+  for (i = 1; i <= 215; i++) {
+    filled[i] = i;
+  }
+  return filled;
+}
 
-
-
-//Next Problem
-
+//
 
 var numbers = [5, '9', 16, 19, '25', '34', 48];
 //Write a function called addTen that is given 'numbers' as it's only argument and returns a new
@@ -128,11 +129,12 @@ var numbers = [5, '9', 16, 19, '25', '34', 48];
 function addTen(numbers) {
    var newArray = [];
    for(var i = 0; i < numbers.length; i++) {
-     newArray.push(numbers[i] + 10); 
+    newArray.push(numbers[i] + 10);
    }
    return newArray;
- }
+}
 
+//
 
 var num1 = Math.floor(Math.random() * (30 - 0) + 0);
 var num2 = Math.floor(Math.random() * (30 - 0) + 0);
@@ -147,23 +149,24 @@ for(var i = 0; i < num2; i++){
 //Above is some code that adds a random number of values to both arr1 and arr2.
 //Write a function that is given arr1 and arr2 as it's only arguments. Return the array which is longest.
 
-  //Code Here
+function longest(arr1, arr2) {
+  if (arr1.length > arr2.length) {
+    return arr1;
+  } else {
+    return arr2;
+  }
+}
+longest(arr1, arr2);
 
 
-/*As a continuation of the previous problem, write another function called 'both'.
-  Your 'both' function will be given two arguments, arr1 and arr2 (from the previous example)
-  'both' should return a new array full of numbers that are found in both arr1 and arr2.
-*/
+/* As a continuation of the previous problem, write another function called 'both'. Your 'both' function will be given two arguments, arr1 and arr2 (from the previous example) 'both' should return a new array full of numbers that are found in both arr1 and arr2. */
 
-  //Code Here
-  
-  
-  
+function both(arr1, arr2) {
+	var arrs = arr1.concat(arr2);
+	return arrs;
+}
 
-//NEXT PROBLEM
-
-
-
+//
 
 var devMountainEmployees = [];
 
@@ -191,31 +194,24 @@ var colt = {
     spiritAnimal: 'Young Male Horse'
 };
 
-/*Above you're given an empty array with four objects. Fill the devMountainEmployees
+/* Above you're given an empty array with four objects. Fill the devMountainEmployees
 array with those four objects. After that console.log the length of the Array and make
 sure that it's equal to 4. */
 
-  //Code Here
+devMountainEmployees.push(tyler, cahlan, ryan, colt);
+console.log(devMountainEmployees.length);
 
-/*Now let's say Cahlan has a mental breakdown and has to take a leave of absence to 'find himself'.
-Loop through your devMountainEmployees until you find cahlan, then remove him from the array.*/
+/* Now let's say Cahlan has a mental breakdown and has to take a leave of absence to 'find himself'. Loop through your devMountainEmployees until you find cahlan, then remove him from the array. */
 
-  //Code Here
-
-
-
-
-//NEXT PROBLEM
-
-
+var charlie = devMountainEmployees.indexOf(cahlan);
+devMountainEmployees.splice(charlie, 1);
+console.log('and in that moment, ' + cahlan.name + ' swore he was infinite...');
+setTimeout(function() {
+  console.log("(it's a 'perks' reference. don't judge me.)")
+}, 5000);
 
 
-/*Now we're going to combine what we've learned today (objects) with what we learned
-yesterday (arrays). Yeah, take a deep breathe. You're ready for this, promise.
-Let's think back to our itunes example (tylermcginnis.com/itunes).
-Notice that when you type in an artist name, iTunes gives us back a LOT of data.
-What they're really giving us is an Array full of Objects. It probably looks something
-like this. */
+/* Now we're going to combine what we've learned today (objects) with what we learned yesterday (arrays). Yeah, take a deep breath. You're ready for this, promise. Let's think back to our iTunes example (tylermcginnis.com/itunes). Notice that when you type in an artist name, iTunes gives us back a LOT of data. What they're really giving us is an Array full of Objects. It probably looks something like this. */
 
 var data = [
     {
@@ -237,17 +233,15 @@ var data = [
 
 
 
-/*A very clean way to pass around large LISTS (arrays) of COLLECTIONS (objects)
-of Data is to have an Array full of objects. */
+/* A very clean way to pass around large LISTS (arrays) of COLLECTIONS (objects)
+of Data is to have an Array full of objects.
+Create an empty array called users. */
 
-//Create an empty array called users.
+var users = [];
 
-  //Code Here
+/* Now add three user objects to your users array. Each user object should contain the
+following properties. name, email, password, username. Include this as one of the objects in your array. */
 
-/*Now add three user objects to your users array. Each user object should contain the
-following properties. name, email, password, username.*/
-
-//include this as one of the objects in your array.
 var user1 = {
     name: 'Tyler McGinnis',
     email: 'tylermcginnis33@gmail.com',
@@ -255,17 +249,30 @@ var user1 = {
     username: 'infiniateLoop'
 };
 
-//Your Code Here
+//
 
-/*Now you have a very common data structure. Twitter is a good use case.
-It's easy to imagine that your followers list on Twitter is an Array full or objects
-and those objects contain properties about the specific person you follow.*/
+var user2 = {
+	name: 'Ryan Walsh',
+	email: 'ryan@thehatrack.net',
+	password: 'one.timeXXX.@bandcamp--lulz',
+	username: 'r-walsh'
+}
 
-/*Now let's say that Tyler decided to delete his account. Loop through your array of
-objects until you find Tyler's account (use tylermcginnis33@gmail.com to find him).
-Once you find the particular index he's located in, delete him from the array.*/
+var user3 = {
+	name: 'Zac Anger',
+	email: 'zac@zacanger.com',
+	password: 'über.h|p$t3R',
+	username: 'zacanger'
+}
 
-  //Code Here
+users.push(user1, user2, user3);
 
-//The activity we just did is very much how data works in 'the real world'.
+/* Now you have a very common data structure. Twitter is a good use case. It's easy to imagine that your followers list on Twitter is an Array full or objects and those objects contain properties about the specific person you follow. Now let's say that Tyler decided to delete his account. Loop through your array of objects until you find Tyler's account (use tylermcginnis33@gmail.com to find him). Once you find the particular index he's located in, delete him from the array. */
 
+var tyler = users.indexOf(['tylermcginnis33@gmail.com']);
+users.splice(tyler, 1);
+
+
+
+
+users.splice.indexOf
