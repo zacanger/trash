9c569165af8e61c29ca81cef0f1276e693f73e0c
zacanger <zac@zacanger.com>
2015-10-28 02:23:18 -0600

    sleep for the next two weeks and get unsick... and chill?

--- js/dataService.js
+++ js/dataService.js
@@ -1,5 +1,5 @@
 var app = angular.module('quoteBook');
-app.service('demQuotes', function(){
+app.service('datService', function(){
   var quotes = [
     { text: 'Life isn\'t about getting and having, it\'s about giving and being.', author: 'Kevin Kruse'},
     { text: 'Whatever the mind of man can conceive and believe, it can achieve', author: 'Napoleon Hill'},
--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,5 +1,5 @@
 var app = angular.module('quoteBook';
-app.controller('mainCtrl', function($scope){
+app.controller('mainCtrl', function($scope datService){
 
 
 });
