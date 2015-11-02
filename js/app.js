64fe847a76b159c4661b7074fe75f7fc05c12471
zacanger <zac@zacanger.com>
2015-11-02 15:40:08 -0700

    i don't understand sports

--- js/app.js
+++ js/app.js
@@ -1,7 +1,24 @@
 var app = angular.module('nbaRoutes', ['ngRoute']);
 
-app.config(function($routeProvider, $httpProvider){
+app.config(function($routeProvider, $httpProvider) {
   $httpProvider.interceptors.push('httpRequestInterceptor');
 
-  //router here
+  $routeProvider
+    .when('/') {
+      templateUrl: 'js/home/homeTmpl.html',
+      controller: 'homeCtrl'
+    }
+    .when('/teams/:team') {
+      templateUrl: 'js/teams/teamTmpl.html',
+      controller: 'teamCtrl',
+      resolve: {
+        teamData: function($route, teamService) {
+          return teamService.getTeamData($route.current.params.team);
+        }
+      }
+    }
+    .otherwise({
+      redirectTo: '/'
+    });
 });
+
--- js/teams/teamCtrl.js
+++ js/teams/teamCtrl.js
@@ -1,5 +1,25 @@
 var app = angular.module('nbaRoutes');
 
-app.controller('teamCtrl', function($scope, $routeParams, teamService){
+app.controller('teamCtrl', function($scope, $routeParams, teamService, teamData){
+  $scope.teamData = teamData;
+  $scope.newGame = {};
+  $scope.showNewGameForm = false;
+  $scope.toggleNewGameForm = function() {
+    $scope.showNewGameForm = !($scope.showNewGameForm);
+  }
+if ($routeParams.team = 'utahjazz') {
+    $scope.homeTeam = 'Utah Jazz';
+    $scope. logoPath = 'images/jazz-logo.png'; }
+if ($routeParams.team = 'losangeleslakers') {
+    $scope.homeTeam = 'Los Angeles Lakers';
+    $scope. logoPath = 'images/lakers-logo.png'; }
+if ($routeParams.team = 'miamiheat') {
+    $scope.homeTeam = 'Miami Heat';
+    $scope. logoPath = 'images/heat-logo.png'; }
+
+
+
 
 });
+
+
