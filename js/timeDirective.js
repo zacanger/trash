6d080efe4ae945c7790e2fd120ff4e4104f04dc1
zacanger <zac@zacanger.com>
2015-11-03 13:16:55 -0700

    okay, this is all borked now. ought to make new branches when i try to show people things... but in theory it would show the time, like, three different ways now, right? right?

--- js/timeDirective.js
+++ js/timeDirective.js
@@ -9,3 +9,26 @@ app.directive('showTime', function() {
         }
     }
 })
+
+
+//angular.module('timeApp');
+//app.directive('showTime', ['$interval', 'dateFilter' function($interval, dateFilter) {
+  //function updateHOURLY() {
+  //element.text(dateFilter(new Date()))
+  //scope.$watch(attrs.currentTime, function(value){
+  //=value;
+  //updateHOURLY();})
+  //timeoutId = $interval(function(){
+  //updateHOURLY();
+  //}, 5);
+  //}
+    //return {
+        //restrict: 'E',
+            //template: '<div> The current time is {{time}} </div>',
+        //link: function(scope, elem, attr) {
+            //var currentTime = new Date();
+            //scope.time = currentTime.toString;
+        //}
+    //}
+//}])
+
