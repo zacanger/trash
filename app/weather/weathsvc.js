8f16805eef3a877acd7d1a857c10b9ae09889903
zacanger <zac@zacanger.com>
2015-11-04 09:27:07 -0700

    can't quite deal with this json correctly, apparently.

--- app/weather/weathsvc.js
+++ app/weather/weathsvc.js
@@ -5,8 +5,8 @@ app.service('weatherService', function ($http, $q) {
     $http.get('http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&mode=json&appid=be22f778633d53500a0fe39dc09ba5d5')
       .then(function (data) {
         deferred.resolve({
-          temperature: data.data.main.temp,
-          weather: data.data.weather[0].description
+          temperature: data.data.list.main,
+          weather: data.data.list.weather
         })
       })
     return deferred.promise
