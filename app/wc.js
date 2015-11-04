34c8ca427d7c21b581d39a35a954c86092244e9d
zacanger <zac@zacanger.com>
2015-11-03 20:16:03 -0700

    that's better.

--- app/wc.js
+++
@@ -1,8 +0,0 @@
-
-
-var app = angular.module('directives');
-
-app.controller('wc', function($scope){
-
-
-http://api.openweathermap.org/data/2.5/forecast?q=%s&units=imperial&mode=xml&appid=be22f778633d53500a0fe39dc09ba5d5
