454d82b08c304a81b5c3dce4a5ef1417d9b34e0c
zacanger <zac@zacanger.com>
2015-11-13 11:45:15 -0700

    32a

--- js/ctrl.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('userProfiles');
-app.controller('mainCtrl', function ($scope, thatService) {
-  $scope.getUsers = function() {
-    return thatService.getUsers();
-    };
-  $scope.users = $scope.getUsers;
-});
