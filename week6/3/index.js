5ee15f032912c673a15f87d208db38ace0ab1643
zacanger <zac@zacanger.com>
2015-11-16 12:33:26 -0700

    asdf# with '#' will be ignored, and an empty message aborts the commit.

--- week6/3/index.js
+++ week6/3/index.js
@@ -1,22 +1,21 @@
-var express = require('express')
-	, bodyparser = require('body-parser')
-	, session = ('express-session')
-	, port = 3000
-	, app = express()
+var express     = require('express')
+  , bodyparser  = require('body-parser')
+  , session     = ('express-session')
+  , port        = 3000
+  , app         = express()
 
 app.use(bodyparser.json())
 
-appuse(session({
-	secret: 'asdfjkl;;'
-	saveUninitialized: true,
-	resave: true
+app.use(session({
+  secret: 'asdfjkl;;',
+  saveUninitialized: true,
+  resave: true
 }))
 
 app.post('/cart', function(req, res, next){
-	if (!req.session){
-		req.session.cart = []
-	}
-	cart.push(req.body)
-	res.status(200).json(cart)
-
+  if (!req.session){
+    req.session.cart = []
+  }
+  cart.push(req.body)
+  res.status(200).json(cart)
 })
