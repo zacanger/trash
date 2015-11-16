2c3323f218f31fa5bc3cb9f4da1efa55b4deabba
zacanger <zac@zacanger.com>
2015-11-16 12:00:34 -0700

    hmph.

--- week6/2/index.js
+++ week6/2/index.js
@@ -1,10 +1,19 @@
 var express = require('express')
+  , session = require('express-session')
   , app = express()
+	, bodyparser = require('body-parser')
   , UsersCtrl = require ('./controllers/UsersCtrl')
   , MoviesCtrl = require('./controllers/MoviesCtrl')
   , cors = require('cors')
+  , port = 3000
 
-app.use(bodyParser.json())
+app.use(bodyparser.json())
+
+app.use(session({
+  secret: 'asdfjkl;;',
+  saveUninitialized: true,
+  resave: true
+}))
 
 app.get('/users', UsersCtrl.index)
 app.get('/users/:id', UsersCtrl.show)
@@ -12,13 +21,17 @@ app.post('/users', UsersCtrl.build)
 app.put('/users/:id', UsersCtrl.update)
 app.delete('/users/:id', UsersCtrl.destroy)
 
-// app.get('/movies', MoviesCtrl.index);
-// app.get('/movies/:id', MoviesCtrl.show);
-// app.post('/movies', MoviesCtrl.build);
-// app.put('/movies/:id', MoviesCtrl.update);
-// app.delete('/movies/:id', MoviesCtrl.destroy);
+app.post('/cart', function(req, res, next){
+	req.session.cart.push(req.body)
+	res.status(200).json(req.session.cart)
+})
 
-var port = 3000;
-app.listen(port, function() {
-  console.log('listening on port: ', port);
-});
+// app.get('/movies', MoviesCtrl.index)
+// app.get('/movies/:id', MoviesCtrl.show)
+// app.post('/movies', MoviesCtrl.build)
+// app.put('/movies/:id', MoviesCtrl.update)
+// app.delete('/movies/:id', MoviesCtrl.destroy)
+
+app.listen(port, function(){
+  console.log('welcome, master. ' +  port + ' is ready for you.')
+})
---
+++ week6/3/index.js
@@ -0,0 +1,22 @@
+var express = require('express')
+	, bodyparser = require('body-parser')
+	, session = ('express-session')
+	, port = 3000
+	, app = express()
+
+app.use(bodyparser.json())
+
+appuse(session({
+	secret: 'asdfjkl;;'
+	saveUninitialized: true,
+	resave: true
+}))
+
+app.post('/cart', function(req, res, next){
+	if (!req.session){
+		req.session.cart = []
+	}
+	cart.push(req.body)
+	res.status(200).json(cart)
+
+})
