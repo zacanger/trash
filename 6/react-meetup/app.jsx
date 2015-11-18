89619461b23606ce40b25c5b1dcbe5c5bced433a
zacanger <zac@zacanger.com>
2015-11-17 22:17:25 -0700

    react react-meteor

---
+++ 6/react-meetup/app.jsx
@@ -0,0 +1,32 @@
+import React from 'react' // same as var React = require('react')
+import ReactDOM from 'react-dom'
+import List from './List'
+import Friendz from './Friendz'
+
+const App = React.createClass({
+  getInitialState(){
+    return (
+      bros: ['bman', 'brosef']
+      )
+    },
+	componentDidMount(){}, // onload
+	componentWillMount(){}, // not quite on load?
+	addFriend(newFriend){
+		this.setState({
+			friends: this.state.friends.cocat(new, newFriend)
+		})
+	}
+
+  render() {
+    return (
+      <div>what up {this.state.bros}</div>
+	)
+  }
+})
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
+
