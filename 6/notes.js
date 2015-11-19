d336b8a0d89a4b673d95e8b8b33a879ccb654030
zacanger <zac@zacanger.com>
2015-11-19 13:42:14 -0700

    i hope they don't make a prequel...

--- 6/notes.js
+++ 6/notes.js
@@ -1,40 +1,38 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var session = require('express-session');
+var express = require('express')
+  , bodyparser = require('body-parser')
+  , session = require('express-session')
+  , app = express()
+  , port = 9999
 
+var logger = function(req, res, next){
+  console.log('\n\n')
+  console.log('\nHEADERS\n', req.headers)
+  console.log('\nBODY\n', req.body)
+  console.log('\nSESSION\n', req.session)
+  next()
+}
 
-var app = express();
-
-var logger = function(req, res, next) {
-  console.log('\n\n------------------------------\n------------------------------\n\n');
-  console.log('\n___HEADERS___\n', req.headers);
-  console.log('\n___BODY___\n', req.body);
-  console.log('\n___SESSION___\n', req.session);
-  next();
-};
-
-app.use(bodyParser.json());
+app.use(bodyparser.json())
 app.use(session({
-  secret: 'qwlwfjkpq3498vsda',
+  secret: 'barFOO',
   saveUninitialized: true,
   resave: true
-}));
+}))
 
-app.post('/cart', function(req, res, next) {
-  if (!req.session.cart) {
-    req.session.cart = [];
+app.post('/cart', function(req, res, next){
+  if (!req.session.cart){
+    req.session.cart = []
   }
-  req.session.cart.push(req.body);
-  next();
-}, logger, function(req, res, next) {
-  res.status(200).json(req.session.cart);
-});
+  req.session.cart.push(req.body)
+  next()
+}, logger, function(req, res, next){
+  res.status(200).json(req.session.cart)
+})
 
-app.get('/cart', function(req, res, next) {
-  res.status(200).json(req.session.cart);
-});
+app.get('/cart', function(req, res, next){
+  res.status(200).json(req.session.cart)
+})
 
-app.listen(3000, function() {
-  console.log('listening to port 3000');
-});
-
+app.listen(port, function(){
+  console.log('doin it on ' + port)
+})
