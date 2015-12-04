a36014127bca76bcf032a07ace600adf952499a2
zacanger <zac@zacanger.com>
2015-12-03 17:12:32 -0700

    pulling out all the placeholder (read: angular-materialize) css

--- working-cms-oneuser/public/home/js/app.js
+++ working-cms-oneuser/public/home/js/app.js
@@ -1,9 +1,8 @@
 var app = angular.module('mean-blog.home', [
   'ui.router',
-  'ngMaterial'
 ])
 
-app.config(function ($stateProvider, $urlRouterProvider) {
+app.config(function($stateProvider, $urlRouterProvider){
   $stateProvider
     .state('home', {
       url: '/',
