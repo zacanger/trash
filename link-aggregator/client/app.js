c6e0a6bf21a0d84b97d1f3d85ff2fb417e021735
zacanger <zac@zacanger.com>
2015-12-07 00:31:11 -0700

    foo blargh

--- link-aggregator/client/app.js
+++ link-aggregator/client/app.js
@@ -55,5 +55,16 @@ app.controller('mainCtrl', ['$scope', 'posts',
 app.controller('postCtrl', ['$scope', '$stateParams', 'posts',
 	function($scope, $stateParams, posts){
 		$scope.post = posts.posts[$stateParams.id]
+
+		$scope.addComment = function(){
+			if ($scope.body === ''){return}
+			$scope.post.comments.push({
+				body: $scope.body
+			, author: 'user'
+			, upvotes: 0
+			})
+			$scope.body = ''
+		}
 	}
 ])
+
