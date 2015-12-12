4b567512b7f6d19a380503ebba6301b1725b1ee1
zacanger <zac@zacanger.com>
2015-12-12 16:55:58 -0700

    ycombinator in js

---
+++ ycomb.js
@@ -0,0 +1,11 @@
+function Y(X){
+  return (function(procedure){
+    return X(function(arg){
+      return procedure(procedure)(arg)
+    })
+  })(function(procedure){
+    return X(function(arg){
+      return procedure(procedure)(arg)
+    })
+  })
+}
