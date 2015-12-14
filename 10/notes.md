862bb31694c26dfb2c55f9c983b728d4874d3d7c
zacanger <zac@zacanger.com>
2015-12-14 16:28:53 -0700

    reactively reacting, having reactions, all to react.

---
+++ 10/notes.md
@@ -0,0 +1,5 @@
+react is reacty. it reacts, sometimes reactively.
+
+it's still ugly as hell and i hope it dies, though.
+
+
---
+++ 10/react-day-1/webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+		entry: './src/App.js'
+	, output: {
+			path: './public/js'
+		, filename: 'bundle.js'
+	},
+	module: {
+		loaders: [{
+			test: /\.js$/, // all files that end in '.js' will be ran through this loader
+			include: /src/, // only run through files located in the `src` directory
+			exclude: /node_modules/, // ignore the `node_modules` directory
+			loader: 'babel?presets[]=react'
+
+		}]
+	}
+}
+
