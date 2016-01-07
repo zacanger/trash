3fea439864235b67a461c11f352b58b134cabaa4
zacanger <zac@zacanger.com>
2016-01-07 11:57:25 -0700

    foo

---
+++ build-process-lg/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react'
+
+const App = React.createClass({
+  render () {
+    return (
+          <div>foo</div>
+    )
+  }
+})
+
+export default App
---
+++ build-process-lg/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { App } from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
