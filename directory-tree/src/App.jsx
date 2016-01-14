d38548ca0f750de91425ad3777b7cdf2f96b2b66
zacanger <zac@zacanger.com>
2016-01-14 16:17:45 -0700

    foo

--- directory-tree/src/App.jsx
+++ directory-tree/src/App.jsx
@@ -1,29 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import Folder from './Folder';
-
-import folders from './sampleFolderStructure';
+import React    from 'react'
+import ReactDOM from 'react-dom'
+import Folder   from './Folder'
+import folders  from './sampleFolderStructure'
 
 const App = React.createClass({
-    getInitialState: function() {
-        return {
-            isOpen: {}
-        }
-    },
-    handleToggle (id) {
-        let oldVal = this.state.isOpen[id];
-        let newState = this.state.isOpen;
-        newState[id] = oldVal ? false : true;
-        this.setState({
-            isOpen: newState
-        })
-    },
-    render: function () {
-        return (
-            <Folder folderDetails={folders.folder} handleToggle={this.handleToggle} isOpen={this.state.isOpen}/>
-        )
+  getInitialState: function(){
+    return {
+      isOpen: {}
     }
-});
+  },
+  handleToggle (id) {
+    let oldVal = this.state.isOpen[id]
+    let newState = this.state.isOpen
+    newState[id] = oldVal ? false : true
+    this.setState({
+      isOpen: newState
+    })
+  },
+  render: function(){
+    return (
+      <Folder folderDetails={folders.folder} handleToggle={this.handleToggle} isOpen={this.state.isOpen}/>
+    )
+  }
+})
 
-export default App;
+export default App
+
