efaea5768d7afb6241d034a2c75e93ba93118d46
zacanger <zac@zacanger.com>
2016-02-08 23:55:01 -0700

    foo

--- no-more-console-log.js
+++ no-more-console-log.js
@@ -1,14 +1,15 @@
 var debug = true
-
+// set debug = false to disable logging
 var _log = function(){
-    debug && window.console && console.log.apply(console, arguments)
+    debug && console && console.log.apply(console, arguments)
   }
 
-function whatever(things){
-  _log(things)
+// example usage in fn
+function ex(ample){
+  _log(ample)
 }
-
 whatever('testing from fn\n')
 
+// example direct useage
 _log('this is testing directly\n')
 
