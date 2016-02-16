46837f9488203dac02c597d5dff70c4ca7117745
zacanger <zac@zacanger.com>
2016-02-16 04:18:53 -0700

    foo

--- boilerplate-hotloader/webpack.config.js
+++ boilerplate-hotloader/webpack.config.js
@@ -1,26 +1,27 @@
-var path = require('path')
+var path    = require('path')
   , webpack = require('webpack')
 
 module.exports = {
-  devtool: 'eval',
-  entry: [
+  devtool : 'cheap-module-eval',
+  entry : [
     'webpack-dev-server/client?http://localhost:4444'
-	, 'webpack/hot/only-dev-server',
-  , './src/index'
+  , 'webpack/hot/only-dev-server'
+  , './src/index.jsx'
   ],
-  output: {
-    path: path.join(__dirname, 'dist')
-	, filename: 'bundle.js'
-	, publicPath: '/static/'
+  output : {
+    path       : './'
+  , filename   : 'bundle.js'
+  , publicPath : './'
   },
-  plugins: [
+  plugins : [
     new webpack.HotModuleReplacementPlugin()
   ],
-  module: {
-    loaders: [{
-      test: /\.js$/
-		, loaders: ['react-hot', 'babel']
-		, include: path.join(__dirname, 'src')
+  module : {
+    loaders : [{
+      test    : /\.jsx$/
+    , loaders : ['react-hot', 'babel']
+    , include : path.join(__dirname, 'src')
     }]
   }
 }
+
