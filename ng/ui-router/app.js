956a4082f1f6d9ac844f386adc812f42109c9519
zacanger <zac@zacanger.com>
2016-02-22 10:56:02 -0700

    generator

---
+++ ng/ui-router/app.js
@@ -0,0 +1,10 @@
+angular.module('app', ['ui-router'])
+.config(function($urlRouteProvider, $stateProvider){
+  $stateProvider
+  .state('home', {
+    url        : '/home'
+  , controller : 'ctrl'
+  , template   : '<em>hello</em>'
+  })
+})
+
