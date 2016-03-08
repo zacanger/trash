655e8715c9ac70c0131e8a8b1ff8f57a1689d891
zacanger <zac@zacanger.com>
2016-03-07 23:59:52 -0700

    foo

--- tiniest-server-plus-plus.js
+++ tiniest-server-plus-plus.js
@@ -4,7 +4,7 @@ var http = require('http')
   , fs   = require('fs')
   , port = process.argv[2] || 9999
 
-http.createServer(function(request, respons){
+http.createServer(function(request, response){
   var uri      = url.parse(request.url).pathname
     , filename = path.join(process.cwd(), uri)
 
