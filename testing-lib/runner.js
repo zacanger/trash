aefd1695d0d30f38f5c5ce0f32b9b49dde178742
zacanger <zac@zacanger.com>
2016-03-13 22:42:11 -0600

    :statue_of_liberty: tiny little testing lib in es6 wut wut

---
+++ testing-lib/runner.js
@@ -0,0 +1,56 @@
+'use strict'
+
+var
+  ran     = 0
+, pending = 0
+
+function assertionError(){}
+
+assertionError.prototype.toString = () => 'assertion error'
+
+module.exports = {
+
+  assert(expression){
+    if(!expression){
+      throw new assertionError
+    } else {
+      return true
+    }
+  },
+
+  describe(name, callable){
+    console.log(name)
+    callable()
+  },
+
+  it(name, callable){
+    console.log(' - ' + name)
+    callable()
+    ran = ran + 1
+  },
+
+  xit(name, callable){
+    console.log('  (PENDING) ' + name)
+    pending = pending + 1
+  },
+
+  before(callable){callable()},
+
+  after(callable){callable()},
+
+  assert_raises(callable){
+    var error = false
+    try {
+      callable()
+    } catch(err){
+      error = true
+    }
+    this.assert(error)
+  },
+
+  summary(){
+    console.log('\nran ' + ran + ' specs (' + pending + ' pending)')
+  }
+
+}
+
