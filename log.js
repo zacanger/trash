0f76a6cf8c66cdf7b4cee80ec113a07e9eab9869
zacanger <zac@zacanger.com>
2016-03-13 21:24:45 -0600

    :rabbit2: log.js (was no-more-console-log.js) es6ified a bit

---
+++ log.js
@@ -0,0 +1,20 @@
+const doLog = true
+
+function log(){
+  if(doLog && console){
+    console.log.apply(console, arguments)
+  }
+}
+
+// example usage
+function ex(ample){
+  log(ample)
+}
+ex('testing from inside a function, here\n')
+log('testing directly, here\n')
+
+// or, put another way:
+// var doLog = true
+// var log = function(){
+//   doLog && console && console.log.apply(console, arguments)
+// }
