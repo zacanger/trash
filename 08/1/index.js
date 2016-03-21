967a6d1178440d1728152e520da1576a3d6652a2
zacanger <zac@zacanger.com>
2016-03-21 10:57:16 -0600

    :fu: what the fuck

--- 08/1/index.js
+++ 08/1/index.js
@@ -1,41 +1,44 @@
-var express = require('express')
-	, session = require('express-session')
-	, passport = require('passport')
-	, Facebook = require('passport-facebook').Strategy
-	, app = express()
-	, port = 3000
+'use strict'
+
+const
+  express  = require('express')
+, session  = require('express-session')
+, passport = require('passport')
+, Facebook = require('passport-facebook').Strategy
+, app      = express()
+, port     = process.env.PORT || 3000
 
 app.use(passport.initialize())
 app.use(passport.session())
 
 passport.use(new Facebook({
-	, callbackURL: 'http://localhost:3000/auth/facebook/callback'
-}, function(token, refreshToken, profile, done){
-	return done(null, profile)
+, callbackURL  : 'http://localhost:3000/auth/facebook/callback'
+}, (token, refreshToken, profile, done) => {
+  return done(null, profile)
 }))
 
 app.get('/auth/facebook', passport.authenticate('facebook'))
 app.get('/auth/facebook/callback', passport.authenticate('facebook', {
-		successRedirect: '/'
-	, failureRedirect: '/login'
-}), function(req, res){
-	console.log(req.session)
-	console.image('http://zacanger.com/gifland/ferret-fail.gif')
+  successRedirect : '/'
+, failureRedirect : '/login'
+}), (req, res) => {
+  console.log(req.session)
+  console.image('http://zacanger.com/gifland/ferret-fail.gif')
 })
 
-passport.serializeUser(function(user, done){
-	done(null, user)
+passport.serializeUser((user, done) => {
+  done(null, user)
 })
 
-passport.deserializeUser(function(obj, done){
-	done(null, obj)
+passport.deserializeUser((obj, done) => {
+  done(null, obj)
 })
 
-app.get('/me', function(req, ers){
-	var currentUser = req.session.user
-	res.send(currentUser)
+app.get('/me', (req, ers) => {
+  var currentUser = req.session.user
+  res.send(currentUser)
 })
-app.listen(port, function(){
-	console.log('oi oi oi at ' + port)
+app.listen(port, () => {
+  console.log('oi oi oi at ' + port)
 })
 
