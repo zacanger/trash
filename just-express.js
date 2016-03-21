c2359d23dd9bf745aa0bcc643b874c9d7bb1d212
zacanger <zac@zacanger.com>
2016-03-20 20:39:23 -0600

    :wavy_dash: just-express, moustache server, scale-fix, xor

--- just-express.js
+++ just-express.js
@@ -1,12 +1,17 @@
-/* only dependency: express */
-var express = require('express')
-  , app     = express()
-  , port    = 3000
+'use strict'
 
-app.get('/', function(req, res){res.send('foo')})
-var server  = app.listen(port, function(){
-  var host = server.address().address
-    , port = server.address().port
+/* only dependency: express */
+const
+  express = require('express')
+, app     = express()
+, port    = 3000
+
+app.get('/', (req, res) => {res.send('foo')})
+
+let server = app.listen(port, () => {
+  let
+    host = server.address().address
+  , port = server.address().port
   console.log('yeah, yo, at http://%s:%s', host, port)
 })
 
---
+++ xor.js
@@ -0,0 +1,9 @@
+// xor(true, true) // => false
+// xor(1, '')      // => true
+
+function xor(x, y){
+  return(!x != !y)
+}
+
+module.exports = xor
+
