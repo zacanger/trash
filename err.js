3ed5813559179de101774abd89f349c36b879dbe
zacanger <zac@zacanger.com>
2016-03-25 15:18:14 -0600

    :u7533: err.js

---
+++ err.js
@@ -0,0 +1,12 @@
+// use instead of `console.error()`; logs to file and stdout both
+
+const
+  fs   = require('fs')
+, util = require('util')
+, file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'})
+, sout = process.stdout
+, err  = d => {
+  file.write(util.format(d) + '\n')
+  sout.write(util.format(d) + '\n')
+}
+
