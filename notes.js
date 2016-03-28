7039bb9b08b49d4bd23c526c21be3af221c4776b
zacanger <zac@zacanger.com>
2016-03-28 13:54:59 -0600

    :ledger: notes

--- notes.js
+++ notes.js
@@ -852,6 +852,31 @@ prom.then(result => {
 })
 
 
+console.log('promise started')
+function myPromise(random){
+  return new Promise(
+    function(resolve, reject){
+      console.log('promise started')
+      function decideStatus(){
+        // resolve with fulfilled if random number > 0.5
+        // reject otherwise
+        (random > 0.5) ? resolve('fulfilled') : reject('rejected')
+      }
+      setTimeout(decideStatus, random * 3000)
+    }
+  )
+}
+// resolve -- then; reject -- catch
+myPromise(math.random())
+.then(
+  function(status){console.log('promise ' + status)
+})
+.catch(function(status){
+  console.log(status)
+})
+console.log('promise finished')
+
+
 
 
 // symbols
@@ -901,5 +926,4 @@ let NegativeIndices = (array) => {
 // Negative array indices:
 // array = NegativeIndices [4, 420, 42]
 // array[-1] is 42
-
 
