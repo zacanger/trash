b62714d389ae980270180123a705ef5531890978
zacanger <zac@zacanger.com>
2016-03-30 15:45:12 -0600

    :birthday: jspm example

--- jspm-example.js
+++ jspm-example.js
@@ -1,5 +1,13 @@
-// jajaja nope not really
-// like browserify, we don't really config things, we just run them.
-// like npm (actually, replacing the need for npm...), we init projects
-// and get shit going and install shit all with jspm.
-// much more sensible than goddamn bower, neh?
+// jspm:
+// npm i -g jspm
+// cd foo
+// jspm init
+// jspm install npm:lodash-node
+// jspm install github:components/jquery
+// jspm install foo (from JSPM's actual registry)
+// in browser:
+// <script src="jspm_packages/system.js"></script>
+// <script src="config.js"></script>
+// <script>System.baseURL='/'
+// System.import('lib/main.js')</script>
+
