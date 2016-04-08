16f25e66bd01ecf8778f0760abf066781406da5b
zacanger <zac@zacanger.com>
2016-04-07 18:06:22 -0600

    foo

--- unit-testing/public/app.js
+++ unit-testing/public/app.js
@@ -3,27 +3,28 @@ angular.module('ecommerce', ['ui.router'])
   $urlRouterProvider.otherwise('/')
 
   $stateProvider
-    .state('home', {
-      url: '/',
-      templateUrl: './views/home.html',
-      controller: 'mainCtrl'
-    })
-    .state('products', {
-      url: '/products',
-      templateUrl: './views/products.html',
-      controller: 'productsCtrl',
-      resolve: {
-        products: function(productsService) {
-          return productsService.getProducts();
-        }
+  .state('home', {
+    url         : '/'
+  , templateUrl : './views/home.html'
+  , controller  : 'mainCtrl'
+  })
+  .state('products', {
+    url         : '/products'
+  , templateUrl : './views/products.html'
+  , controller  : 'productsCtrl'
+  , resolve : {
+      products : function(productsService) {
+        return productsService.getProducts()
       }
-    })
-    .state('admin', {
-      url: '/admin',
-      templateUrl: './views/admin.html',
-      controller: 'adminCtrl'
-    })
-    .state('admin.edit', {
-      templateUrl: './views/adminEdit.html'
-    })
+    }
+  })
+  .state('admin', {
+    url         : '/admin'
+  , templateUrl : './views/admin.html'
+  , controller  : 'adminCtrl'
+  })
+  .state('admin.edit', {
+    templateUrl : './views/adminEdit.html'
+  })
 })
+
--- unit-testing/public/controllers/productsCtrl.js
+++ unit-testing/public/controllers/productsCtrl.js
@@ -1,4 +1,5 @@
 angular.module('ecommerce')
 .controller('productsCtrl', function($scope, products) {
-  $scope.products = products;
+  $scope.products = products
 })
+
