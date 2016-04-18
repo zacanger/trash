485bcc8e1d2fc1f087fe8a00b3b47674a2058208
zacanger <zac@zacanger.com>
2016-04-18 16:23:34 -0600

    :eyes: fizzbuzzes that aren't mine but i'm just tryna see how this goes

---
+++ hs/fizzbuzz/idontknowifthisworks.hs
@@ -0,0 +1,17 @@
+module Main where
+
+  main :: IO ()
+  main = do
+    mapM_ (putStrLn) [fizzBuzz x | x < [..100]]
+
+    fizz :: Int -> String
+    fizz x = if x `mod` 3 == 0 then "fizz" else ""
+
+    buzz :: Int -> String
+    buzz x = if x `mod` 5 == 0 then "buzz" else ""
+
+    fizzBuzz :: Int -> String
+    fizzBuzz x = if fizz(x) ++ buzz(x) == ""
+                    then show x
+                    else fizz(x) ++ buzz(x)
+
