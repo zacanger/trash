b5b4cee9cbcf8e6f265a061b483367f4e6d6a211
zacanger <zac@zacanger.com>
2016-04-30 23:54:29 -0600

    :package: moving fp to wkshprs

--- fp/hello.js
+++
@@ -1,6 +0,0 @@
-const up = str => str.toUpperCase()
-
-module.exports = up
-
-console.log(up('hello!'))
-
--- fp/reduce.js
+++
@@ -1,9 +0,0 @@
-// take arr of strs, return obj that contains number of times each str is in array.
-
-const strs = ['one', 'one', 'two', 'one', 'four', 'three', 'three']
-
-const countStrs = arr => arr.reduce((prev, curr) => {
-  prev[curr] = ++prev[curr] || 1
-  return prev
-}, {})
-
