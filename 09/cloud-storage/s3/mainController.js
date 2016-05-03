54f6ed1031041ef6f6882fc75a752f73e1df6358
zacanger <zac@zacanger.com>
2016-05-02 20:54:35 -0600

    foo

--- 09/cloud-storage/s3/mainController.js
+++ 09/cloud-storage/s3/mainController.js
@@ -1,32 +1,37 @@
-var AWS = require('aws-sdk')
-  , Keys = require('./keys.js')
+const
+  AWS     = require('aws-sdk')
+, Keys    = require('./keys.js')
+, s3      = new AWS.S3()
+
+let exports = module.exports = {}
 
 AWS.config.update({
-    accessKeyId: Keys.amazonAccess
-  , secretAccessKey: Keys.amazonSecret
-  , region: Keys.amazonRegion
+  accessKeyId     : Keys.amazonAccess
+, secretAccessKey : Keys.amazonSecret
+, region          : Keys.amazonRegion
 })
 
-var s3 = new AWS.S3()
-  , exports = module.exports = {}
 
-exports.saveImage = function(req, res){
-  buf = new Buffer(req.body.imageBody.replace(/^data:image\/\w+;base64,/, ''), 'base64')
+exports.saveImage = (req, res) => {
+  let buf = new Buffer(req.body.imageBody.replace(/^data:image\/\w+;base64,/, ''), 'base64')
 
   // bucketName var below crates a "directory" for each user
-  var bucketName = 'mailpants/' + req.body.userEmail
-  var params = {
-      Bucket: bucketName
-    , Key: req.body.imageName
-    , Body: buf
-    , ContentType: 'image/' + req.body.imageExtension
-    , ACL: 'public-read'
+  let bucketName = 'demo/' + req.body.userEmail
+  let params = {
+      Bucket      : bucketName
+    , Key         : req.body.imageName
+    , Body        : buf
+    , ContentType : 'image/' + req.body.imageExtension
+    , ACL         : 'public-read'
   }
 
-  s3.upload(params, function(err, data){
+  s3.upload(params, (err, data) => {
     console.log(err, data)
-    if (err) return res.status(500).send(err)
+    if (err) {
+      return res.status(500).send(err)
+    }
     // this is where we'd actually deal with this data, save the metadata to mongo, etc.
     res.json(data)
   })
 }
+
--- 09/cloud-storage/s3/server.js
+++ 09/cloud-storage/s3/server.js
@@ -1,27 +1,28 @@
-var express = require('express')
-  , bodyParser = require('body-parser')
-  , cors = require('cors')
-  , mongoose = require('mongoose')
-  , app = express()
-  , portNum = 3000
-  , Controller = require('./mainController.js')
-  , mongooseUri = 'mongodb://127.0.0.1:27017/mailpants'
-
-app.use(express.static(__dirname + '/'))
-app.use(cors())
-app.use(bodyParser.json({limit: '50mb'}))
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
-
-app.post('/api/newimage', Controller.saveImage)
+const
+  express    = require('express')
+, bodyParser = require('body-parser')
+, cors       = require('cors')
+, mongoose   = require('mongoose')
+, app        = express()
+, port       = process.env.PORT || 3000
+, Controller = require('./mainController.js')
+, mongooseUri = 'mongodb://127.0.0.1:27017/s3'
 
 mongoose.connect(mongooseUri)
 
-var db = mongoose.connection
+let db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
-db.once('open', function(callback){
+db.once('open', callback => {
   console.log('mongoose is over at ', mongooseUri)
 })
 
-app.listen(portNum, function(){
-  console.log('and we\'re listening on ', portNum)
+app
+.use(express.static(__dirname + '/'))
+.use(cors())
+.use(bodyParser.json({limit : '50mb'}))
+.use(bodyParser.urlencoded({limit : '50mb', extended : true}))
+.post('/api/newimage', Controller.saveImage)
+.listen(port, () => {
+  console.log(`listening on ${port}`)
 })
+
