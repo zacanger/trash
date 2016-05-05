ccbc45219f60177894a560e1489cb87c327de45b
zacanger <zac@zacanger.com>
2016-05-04 20:31:42 -0600

    foo

--- passport-local.js
+++ passport-local.js
@@ -20,12 +20,8 @@ var users = [
 ]
 
 passport
-.serializeUser((user, done) => {
-  done(null, users[0].id)
-})
-.deSerializeUser((id, done) => {
-  done(null, users[0])
-})
+.serializeUser((user, done) => done(null, users[0].id))
+.deSerializeUser((id, done) => done(null, users[0]))
 .use('local-login', new Local((username, password, done) => {
   if(username === users[0].username && password === users[0].password){
     return done(null, users[0])
@@ -46,7 +42,7 @@ app
 .use(passport.session())
 
 function isLoggedIn(req, res, next){
-  if(req.isAuthenticated()){
+  if (req.isAuthenticated()) {
     return next()
   }
   res.sendStatus(401)
@@ -54,15 +50,21 @@ function isLoggedIn(req, res, next){
 
 app
 .get('/', (req, res) => {res.send('howdy')})
-.get('/login', (req, res) => {res.send(
-'<p>login!</p><form method="post" action="/login"><input type="text" name="username"><input type="password" name="password"><button type="submit" value="submit">Submit</buttom></form>'
-)})
-.post('/login', passport.authenticate('local-login', {failureRedirect : '/login'}, (req, res) => {res.redirect('/content')}))
-.get('/content', isLoggedIn, (req, res) => {res.send('logged in!')})
+.get('/login', (req, res) => {res.send(`
+  <p>login!</p>
+  <form method="post" action="/login">
+    <input type="text" name="username">
+    <input type="password" name="password">
+    <button type="submit" value="submit">Submit</button>
+  </form>`)})
+
+.post('/login', passport.authenticate('local-login', {failureRedirect : '/login'}, (req, res) => res.redirect('/content')))
+.get('/content', isLoggedIn, (req, res) => res.send('logged in!'))
 .get('/logout', (req, res) => {
   req.logout()
   res.send('logged out!')
 })
 .listen(port)
 
-console.log('listening on ' + port)
+console.log(`listening on ${port}`)
+
