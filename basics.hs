4e9921623fb70b794817db4702302ca9d05c6fb9
zacanger <zac@zacanger.com>
2016-05-19 19:40:59 -0600

    metafiles and moving meetup notes to the bottom of basics

--- basics.hs
+++ basics.hs
@@ -348,3 +348,13 @@ drop 1 "zac" -- "ac"
 "zac" !! 0 -- 'z'
 "zac" !! 2 -- 'c'
 
+-- Integer will expand as much as needed (like a bignum or whatever
+-- that is), within (memory) limits.
+
+-- Int is limited to usually either 32 or 64 (in every implementation,
+-- at least 30) bits.
+
+-- Just x -- this means that x is a thing!
+-- bool is implemented basically as: Bool = True | false
+-- maybe a = Nothing | Just a
+
