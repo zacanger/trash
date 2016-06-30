308d1b249b34d6bf0ae9c9ab0aaab8e694b0f75a
zacanger <zac@zacanger.com>
2016-06-30 13:55:10 -0600

    hmr boilerplate

---
+++ boilerplates/hmr/src/main.js
@@ -0,0 +1,8 @@
+import React    from 'react'
+import ReactDOM from 'react-dom'
+import Counter  from './Counter'
+
+ReactDOM.render(
+  React.createElement(Counter)
+, document.getElementById('root')
+)
