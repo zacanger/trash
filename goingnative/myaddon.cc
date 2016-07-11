258ef1b70a1ff3e8d1703c0aa7a3c6310b17b7fc
zacanger <zac@zacanger.com>
2016-07-11 08:38:10 -0600

    fix

--- goingnative/myaddon.cc
+++ goingnative/myaddon.cc
@@ -6,33 +6,26 @@
 using namespace v8;
 
 class MyWorker : public Nan::AsyncWorker {
- public:
+public:
   MyWorker(Nan::Callback *callback, int delay)
     : Nan::AsyncWorker(callback), delay(delay) {}
   ~MyWorker() {}
 
-  // Executed inside the worker-thread.
-  // It is not safe to access V8, or V8 data structures
-  // here, so everything we need for input and output
-  // should go on `this`.
   void Execute () {
-    #ifdef _WIN32
-     Sleep(delay);
-    #else
-     usleep(delay * 1000);
-    #endif
+#ifdef _WIN32
+    Sleep(delay);
+#else
+    usleep(delay * 1000);
+#endif
   }
 
-  // Executed when the async work is complete
-  // this function will be run inside the main event loop
-  // so it is safe to use V8 again
   void HandleOKCallback () {
     Nan::HandleScope scope;
 
     callback->Call(0, NULL);
   }
 
- private:
+private:
   int delay;
 };
 
@@ -58,7 +51,7 @@ NAN_METHOD(Delay) {
 
 NAN_MODULE_INIT(Init) {
   Nan::Set(target, Nan::New("delay").ToLocalChecked(),
-      Nan::GetFunction(Nan::New<FunctionTemplate>(Delay)).ToLocalChecked());
+           Nan::GetFunction(Nan::New<FunctionTemplate>(Delay)).ToLocalChecked());
 }
 
 NODE_MODULE(myaddon, Init)
