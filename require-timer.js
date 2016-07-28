a7780447c8879fba1fa774caff1e16684a982896
zacanger <zac@zacanger.com>
2016-07-28 16:15:07 -0600

    console-group-node.js

--- require-timer.js
+++ require-timer.js
@@ -1,12 +1,13 @@
 // just require this file to time requires into that file
 // based on gh:stefanpenner/node-require-timings
-const fs = require('fs')
+const { readFileSync } = require('fs')
 require.extensions['.js'] = (module, filename) => {
-  const strt = new Date()
-  const cont = fs.readFileSync(filename, 'utf8').toString()
-  const modl = module._compile(cont, filename)
-  const arry = []
-  const item = `${new Date() - strt} : ${filename}`
+  const
+    strt = new Date()
+  , cont = readFileSync(filename, 'utf8').toString()
+  , modl = module._compile(cont, filename)
+  , arry = []
+  , item = `${new Date() - strt} : ${filename}`
   arry.push(item)
   console.log(arry)
   return modl
