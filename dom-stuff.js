7836e76a88ad52157e892466e9d34006134e389c
zacanger <zac@zacanger.com>
2016-07-29 06:56:11 -0600

    debounce-settimeout-input-jq.js

---
+++ dom-stuff.js
@@ -0,0 +1,10 @@
+const scrollTop = () =>
+  global.scrollTo(0, 0)
+
+const getWidth = () =>
+  global.innerWidth ||
+  global.document.documentElement.clientWidth
+
+const getHeight = () =>
+  global.innerHeight ||
+  global.document.documentElement.clientHeight
