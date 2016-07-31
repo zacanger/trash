1f3d3a1b66523fead269c622ce529bb962633997
zacanger <zac@zacanger.com>
2016-07-31 08:07:45 -0600

    bare bones boilerplate for those in need

---
+++ boilerplates/bare-bones/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Foo from './Foo'
+
+export default class App extends Component {
+  render () {
+    return (
+      <div>
+        <Foo />
+      </div>
+    )
+  }
+}
---
+++ boilerplates/bare-bones/src/Foo.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Foo = () => <span>hi!</span>
+
+export default Foo
