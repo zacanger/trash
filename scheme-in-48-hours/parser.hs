3380c91bbc8cc53b784a94d9e9bb78a02eca5e77
zacanger <zac@zacanger.com>
2016-08-31 20:13:52 -0600

    parser

---
+++ scheme-in-48-hours/parser.hs
@@ -0,0 +1,52 @@
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+import Control.Monad
+
+-- ghc --make -o foo parser.hs
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+-- use >> if you're not returning a val
+-- use >>= if you're passing a val immediately
+-- use `do` otherwise
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do -- an atom is:
+  first <- letter <|> symbol -- one of these
+  rest <- many (letter <|> digit <|> symbol) -- followed by any number of these
+  let atom = first:rest -- cons; could also be [first] ++ rest
+  return $ case atom of
+             "#t" -> Bool True
+             "#f" -> Bool False
+             _    -> Atom atom -- 'default'
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol ) "lisp" input of
+                   Left err -> "No match: " ++ show err
+                   Right val -> "Found value"
+
+main :: IO ()
+main = do
+  (expr:_) <- getArgs
+  putStrLn (readExpr expr)
