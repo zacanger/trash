44530e882d1110fee0d9c673a1e701ce6a1f73d8
zacanger <zac@zacanger.com>
2016-09-04 19:51:32 -0600

    space

--- scheme/stdlib.scm
+++ scheme/stdlib.scm
@@ -51,4 +51,4 @@
 (define (assoc obj alist)    (fold (mem-helper (curry equal? obj) car) #f alist))
 
 (define (map func lst)       (foldr (lambda (x y) (cons (func x) y)) '() lst))
-(define (filter pred lst)   (foldr (lambda (x y) (if (pred x) (cons x y) y)) '() lst))
+(define (filter pred lst)    (foldr (lambda (x y) (if (pred x) (cons x y) y)) '() lst))
