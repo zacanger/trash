203b913cb7ccd21898b6ee9721118ab455af1f87
zacanger <zac@zacanger.com>
2016-10-02 20:12:17 -0600

    just a lil more

--- the-little-schemer/lib.rkt
+++ the-little-schemer/lib.rkt
@@ -8,3 +8,10 @@
       ((null? l) #t)
       ((atom? (car l)) (lat? (cdr l)))
       (else #f))))
+
+(define member?
+  (λ (a lat)
+    (cond
+      ((null? lat) #f)
+      (else (or (eq? (car lat) a)
+                (member? a (cdr lat)))))))
