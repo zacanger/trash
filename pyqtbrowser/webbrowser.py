941a2645913bb0cacca4e9f0a9b35522fe9acb62
zacanger <zac@zacanger.com>
2016-12-23 11:12:54 -0700

    Turns out it _is_ possible to make a halfway-decent widget toolkit work on a Mac. Who knew.

--- pyqtbrowser/webbrowser.py
+++ pyqtbrowser/webbrowser.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python
 
 #############################################################################
-## Copyright 2009 0xLab  
+## Copyright 2009 0xLab
 ## Authored by Erin Yueh <erinyueh@gmail.com>
 ##
 ## Copyright (C) 1992-2009 Trolltech ASA. All rights reserved.
@@ -26,7 +26,7 @@ import sys
 from PyQt4 import QtCore, QtGui, QtWebKit
 from ui_mainwindow import Ui_MainWindow
 from flickcharm import *
-import bookmark 
+import bookmark
 from ui_bookmark import *
 
 ITEM_WIDTH = 300
@@ -41,24 +41,24 @@ class GraphicsView(QtGui.QGraphicsView):
         self.connect(self.reloadTimer, QtCore.SIGNAL("timeout()"),
                      self.redisplayWindow)
         self.reloadTimer.setSingleShot(True)
-        
+
     def mouseReleaseEvent(self, event):
         #print 'mouse Release event'
-        QtGui.QGraphicsView.mouseReleaseEvent(self, event) 
-    
+        QtGui.QGraphicsView.mouseReleaseEvent(self, event)
+
     def redisplayWindow(self):
         #print 'reload URL'
         self.mainwindow.WebBrowser.load(QtCore.QUrl(self.reloadbookmark[1]))
-        
+
     def mousePressEvent(self, event):
         #print 'mouse Press event'
         item = self.itemAt(event.pos())
         #print 'bookmark: ',self.mainwindow.booklist[int(item.zValue())]
         bookmark = self.mainwindow.booklist[int(item.zValue())]
-        self.reloadTimer.start(300)    
+        self.reloadTimer.start(300)
         self.reloadbookmark = bookmark
-        QtGui.QGraphicsView.mousePressEvent(self, event) 
-        
+        QtGui.QGraphicsView.mousePressEvent(self, event)
+
     def mouseDoubleClickEvent(self, event):
         #print 'mouseDoubleClick event'
         if self.reloadTimer and self.reloadTimer.isActive():
@@ -80,8 +80,8 @@ class GraphicsView(QtGui.QGraphicsView):
             item.setZValue(i)
             item.setFlag(QtGui.QGraphicsItem.ItemIsSelectable, True)
             i += 1
-        
-                   
+
+
 class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
     # Maintain the list of browser windows so that they do not get garbage
     # collected.
@@ -93,7 +93,7 @@ class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
         MainWindow._window_list.append(self)
         # finger scrolling effect
         self.charm = FlickCharm()
-        
+
         # create bookmark window
         self.db = bookmark.connect()
         if self.db:
@@ -110,54 +110,54 @@ class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
             item.setZValue(i)
             item.setFlag(QtGui.QGraphicsItem.ItemIsSelectable, True)
             i += 1
-        
+
         self.scene.setItemIndexMethod(QtGui.QGraphicsScene.BspTreeIndex)
         self.bookview.setScene(self.scene)
         self.bookview.setRenderHints(QtGui.QPainter.TextAntialiasing)
         self.bookview.setFrameShape(QtGui.QFrame.NoFrame)
         self.bookview.setWindowTitle("Bookmark List")
         self.charm.activateOn(self.bookview)
-        
+
         # create web window
         self.setupUi(self)
         self.charm.activateOn(self.WebBrowser)
-        
+
         self.lblAddress = QtGui.QLabel("", self.tbAddress)
         self.tbAddress.insertWidget(self.actionGo, self.lblAddress)
         self.addressEdit = QtGui.QLineEdit(self.tbAddress)
         self.tbAddress.insertWidget(self.actionGo, self.addressEdit)
-        
+
         self.addressEdit.setFocusPolicy(QtCore.Qt.StrongFocus)
-        
+
         self.connect(self.addressEdit, QtCore.SIGNAL("returnPressed()"),
                      self.actionGo, QtCore.SLOT("trigger()"))
-                     
+
         self.connect(self.actionBack, QtCore.SIGNAL("triggered()"),
                      self.WebBrowser, QtCore.SLOT("back()"))
-        
+
         self.connect(self.actionForward, QtCore.SIGNAL("triggered()"),
                      self.WebBrowser, QtCore.SLOT("forward()"))
-        
+
         self.connect(self.actionStop, QtCore.SIGNAL("triggered()"),
                      self.WebBrowser, QtCore.SLOT("stop()"))
-        
+
         self.connect(self.actionRefresh, QtCore.SIGNAL("triggered()"),
                      self.WebBrowser, QtCore.SLOT("reload()"))
-                   
+
         self.pb = QtGui.QProgressBar(self.statusBar())
         self.pb.setTextVisible(False)
         self.pb.hide()
         self.statusBar().addPermanentWidget(self.pb)
         self.WebBrowser.load(QtCore.QUrl("http://www.google.com"))
 
-       
+
     @QtCore.pyqtSignature("")
     def on_actionHome_triggered(self):
         self.WebBrowser.load(QtCore.QUrl("http://www.google.com"))
 
     def on_WebBrowser_urlChanged(self, url):
         self.addressEdit.setText(url.toString())
-        
+
     def on_WebBrowser_titleChanged(self, title):
         #print 'titleChanged',title.toUtf8()
         self.setWindowTitle(title)
@@ -167,13 +167,13 @@ class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
         self.pb.show()
         self.pb.setRange(0, 100)
         self.pb.setValue(1)
-        
+
     def on_WebBrowser_loadFinished(self, flag):
         #print 'loadFinished'
         if flag is True:
             self.pb.hide()
             self.statusBar().removeWidget(self.pb)
-            
+
     def on_WebBrowser_loadProgress(self, status):
         self.pb.show()
         self.pb.setRange(0, 100)
@@ -190,14 +190,14 @@ class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
         #print "on_actionZoomIn_triggered"
         current = self.WebBrowser.textSizeMultiplier()
         self.WebBrowser.setTextSizeMultiplier(current+0.1)
-        
+
     @QtCore.pyqtSignature("")
     def on_actionZoomOut_triggered(self):
         #print "on_actionZoomOut_triggered"
         current = self.WebBrowser.textSizeMultiplier()
         self.WebBrowser.setTextSizeMultiplier(current-0.1)
 
-        
+
     @QtCore.pyqtSignature("")
     def on_actionZoomNormal_triggered(self):
         #print "on_actionZoomNormal_triggered"
@@ -208,7 +208,7 @@ class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
         #print "on_actionShowBookmark_triggered"
         self.bookview.show()
         temp = bookmark.refresh(self.db)
-        
+
     @QtCore.pyqtSignature("")
     def on_actionAddBookmark_triggered(self):
         #print "on_actionAddBookmark_triggered"
@@ -221,9 +221,9 @@ class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
         item.setZValue(len(self.booklist))
         item.setFlag(QtGui.QGraphicsItem.ItemIsSelectable, True)
         self.booklist.append(data)
-        self.scene.addItem(item)    
+        self.scene.addItem(item)
         self.scene.update()
-       
+
     @QtCore.pyqtSignature("")
     def on_actionHome_triggered(self):
         #print "on_actionHome_triggered"
