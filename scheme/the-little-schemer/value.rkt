8b0ab134d433fdeee187771488ba1ff0c6c0610e
zacanger <zac@zacanger.com>
2017-01-25 09:24:54 -0700

    Yay.

--- scheme/the-little-schemer/value.rkt
+++ scheme/the-little-schemer/value.rkt
@@ -12,3 +12,12 @@
   (^ (value (1st-sub-exp a))
      (value (2nd-sub-exp a))))
 
+(define val
+  (lambda (a)
+    (cond
+      ((atom? a) a)
+      (else
+        (atom-to-fn
+          (operator a))
+        (val (1st-sub-exp a))
+        (val (2nd-sub-exp a))))))
