e9aee191f2569c06ea10fcd935474800c32b286c
zacanger <zac@zacanger.com>
2017-11-26 15:14:10 -0700

    add max file size note

--- src/pages/home.js
+++ src/pages/home.js
@@ -9,6 +9,7 @@ module.exports = () =>
       <div style="margin-top:32px;">
         <form action="/upload" method="post" enctype="multipart/form-data">
           <div>
+            <span>Max file size: 50MB</span>
             <input type="file" id="ffile" name="file" autocomplete="off">
             <button type="submit">Submit</button>
           </div>
