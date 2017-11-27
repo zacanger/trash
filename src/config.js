f3e96088281fa85c28386b93829fc2fa692c0157
zacanger <zac@zacanger.com>
2017-11-26 17:45:17 -0700

    clean up legal

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const { resolve } = require('path')
 const p = resolve(__dirname, '..', 'imgs')
 
-const legal = 'You may upload any files that are legal under US law. Images will be rendered correctly on the list page. Illegal or copyrighted material takedown requests can be sent to zac at zac anger dot com and will be respected with a DMCA or court order. This software is a free and open source project available under the WTFPL.' // eslint-disable-line max-len
+const legal = 'You may upload any files that are legal under US law. Illegal or copyrighted material takedown requests can be sent to zac at zac anger dot com and will be respected, with a DMCA or court order. This is a free and open source software project available under the WTFPL. Source code can be found at github.com/zacanger/imghost.' // eslint-disable-line max-len
 
 module.exports = {
   imagePath: p,
