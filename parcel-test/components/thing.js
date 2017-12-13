357caa4531320cca4e7de2221bd7f3f01aecfeee
zacanger <zac@zacanger.com>
2017-12-12 18:22:49 -0700

    parcel test

--- parcel-test/components/thing.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-const s = { backgroundColor: 'red' }
-
-export default ({ foo }) =>
-  <span style={s}>{foo}</span>
--- parcel-test/index.js
+++ parcel-test/index.js
@@ -1,10 +1,5 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Thing from './components/thing'
-
-const App = () =>
-  <div>
-    <Thing foo="bar" />
-  </div>
+import App from './components/app'
 
 render(<App />, document.getElementById('root'))
