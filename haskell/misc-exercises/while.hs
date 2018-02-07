730d8eb3dd4761e8384ea0249c96e73e746e1ba6
zacanger <zac@zacanger.com>
2018-02-06 19:22:46 -0700

    while.hs

--- haskell/misc-exercises/while.hs
+++ haskell/misc-exercises/while.hs
@@ -4,3 +4,10 @@ while cond action = do
   if c
      then action >> while cond action
      else return ()
+
+-- another definition without do sugar
+while = \cond body ->
+  cond >>= (\testResult ->
+    if testResult
+       then (body >> while cond body)
+       else (return ()))
