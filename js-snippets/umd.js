610ba28828596cc8819aa83f0e1d14244b792228
zacanger <zac@zacanger.com>
2018-02-27 18:55:36 -0700

    umd.js

---
+++ js-snippets/umd.js
@@ -0,0 +1,4 @@
+;(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) : (global.Split = factory());
+}(this, (function () { 'use strict'; /* module here; return module */ })));
