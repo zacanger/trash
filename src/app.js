5359f84d6437714cfb3aa42f4b60f936920730fd
zacanger <zac@zacanger.com>
2018-10-27 11:45:03 -0600

    Cache in localstorage for a day to avoid rate limiting

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import Repos from './repos'
 
+const storageKey = 'angrytech'
 const globalStyles = `
 html, body {
   margin: 0;
@@ -16,34 +17,61 @@ export default class App extends React.Component {
     this.state = { repos: [] }
   }
 
+  getFromStorage() {
+    try {
+      const data = JSON.parse(localStorage.getItem(storageKey))
+      const now = new Date()
+      const then = new Date(data.date)
+      if ((now - then) / 36e5 <= 24) {
+        return Promise.resolve(data.repos)
+      }
+    } catch (_) {
+      return Promise.resolve([])
+    }
+  }
+
+  setInStorage(repos) {
+    try {
+      localStorage.setItem(storageKey, JSON.stringify({ repos, date: new Date() }))
+    } catch (_) {}
+  }
+
   componentDidMount() {
     const style = document.createElement('style')
     style.innerHTML = globalStyles
     document.head.appendChild(style)
 
-    fetch('https://api.github.com/users/zacanger/repos?sort=updated')
-      .then(b => b.json())
-      .then(rs => {
-        const repos = rs
-          .filter((r) => !r.fork && !r.archived)
-          .map(
-            ({
-              description,
-              html_url: url,
-              language,
-              name,
-              stargazers_count: stars
-            }) => ({
-              description,
-              language,
-              name,
-              stars,
-              url
-            })
-          )
-          .sort((a, b) => b.stars - a.stars)
+    this.getFromStorage()
+      .then((repos) => {
+        if (repos && repos.length) {
+          this.setState({ repos })
+        } else {
+          fetch('https://api.github.com/users/zacanger/repos?sort=updated')
+            .then(b => b.json())
+            .then(rs => {
+              const repos = rs
+                .filter((r) => !r.fork && !r.archived)
+                .map(
+                  ({
+                    description,
+                    html_url: url,
+                    language,
+                    name,
+                    stargazers_count: stars
+                  }) => ({
+                    description,
+                    language,
+                    name,
+                    stars,
+                    url
+                  })
+                )
+                .sort((a, b) => b.stars - a.stars)
 
-        this.setState({ repos })
+              this.setInStorage(repos)
+              this.setState({ repos })
+            })
+        }
       })
   }
 
