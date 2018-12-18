d20373a7e9cc425ec25fc9dc5b383d2954b21ccb
zacanger <zac@zacanger.com>
2018-12-18 11:54:40 -0700

    clean up mt

--- qt/mt/main.cpp
+++ qt/mt/main.cpp
@@ -4,14 +4,6 @@
 #include <QMainWindow>
 #include "qtermwidget.h"
 
-/*
-void activateUrl(const QUrl &url, bool fromContextMenu) {
-  if (QApplication::keyboardModifiers() & Qt::ControlModifier || fromContextMenu) {
-    QDesktopServices::openUrl(url);
-  }
-}
-*/
-
 int main(int argc, char *argv[]) {
   QApplication app(argc, argv);
   QMainWindow *mainWindow = new QMainWindow();
@@ -28,29 +20,8 @@ int main(int argc, char *argv[]) {
   mt->setTerminalSizeHint(false);
   // mt->setTerminalOpacity(0.5);
 
-  // QObject::connect(mt, &QTermWidget::urlActivated, mainWindow, activateUrl);
-
-  // handle copy and paste
-  // disabled, interferes with visual block in vim
-  /*
-  QObject::connect(
-      mt,
-      &QTermWidget::termKeyPressed,
-      mainWindow,
-      [=](const QKeyEvent *key) -> void {
-        if (key->matches(QKeySequence::Copy)) {
-          mt->copyClipboard();
-        } else if (key->matches(QKeySequence::Paste)) {
-          mt->pasteClipboard();
-        }
-      }
-  );
-  */
-
   QObject::connect(mt, SIGNAL(finished()), mainWindow, SLOT(close()));
-
   mainWindow->setCentralWidget(mt);
-
   mainWindow->show();
   return app.exec();
 }
