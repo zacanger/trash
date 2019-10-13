66c419cab4325b3e751be96147804d052d0c8980
zacanger <zac@zacanger.com>
2019-10-13 09:28:17 -0600

    chore: rm gcloud

--- bin/new-linux.sh
+++ bin/new-linux.sh
@@ -136,12 +136,6 @@ sudo dpkg -i keybase_amd64.deb
 sudo apt-get install -f
 run_keybase
 
-# Set up gcloud cli
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-sudo apt-get update && sudo apt-get install google-cloud-sdk -y
-gcloud init
-
 # Cleanup
 apt autoremove
 apt purge
