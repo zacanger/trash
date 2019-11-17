dd692e6f409d9ede3d16eadd58f7d4ac1a2f4110
zacanger <zac@zacanger.com>
2019-11-17 13:10:59 -0700

    misc

--- bin/watchdir.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# watches the passed directory every second and show files in order of latest modified
-watch -n 1 "find $1 -type f -printf \"%TY-%Tm-%Td %TT %p\n\" | sort -r"
