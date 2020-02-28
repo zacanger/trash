974731cc11e04a2e3267e3d8ba3ac0162a9492db
zacanger <zac@zacanger.com>
2020-02-27 18:22:32 -0700

    chore: clean up names.sh

--- bin/names.sh
+++ bin/names.sh
@@ -6,14 +6,25 @@ if [ -z "$1" ]; then
   exit 0
 fi
 
-find "$1" -depth -name '*' | while read file ; do
+find "$1" -depth -name '*' | while read file; do
   directory=$(dirname "$file")
   oldfilename=$(basename "$file")
   # tr -d '[{}(),\!]' | tr -d "\'"
-  newfilename=$(echo "$oldfilename" | tr 'A-Z' 'a-z' | tr ',' '_' | tr '[' '-' | tr ']' '-' | tr "'" '-' | tr '"' '-' | tr ' ' '_' | tr '(' '-' | tr ')' '-' | sed 's/_-_/-/g')
+  newfilename=$(echo "$oldfilename" \
+    | tr 'A-Z' 'a-z' \
+    | tr ',' '_' \
+    | tr '[' '-' \
+    | tr ']' '-' \
+    | tr "'" '-' \
+    | tr '"' '-' \
+    | tr ' ' '_' \
+    | tr '(' '-' \
+    | tr ')' '-' \
+    | sed 's/_-_/-/g')
+
   if [ "$oldfilename" != "$newfilename" ]; then
     mv -i "$directory/$oldfilename" "$directory/$newfilename"
-    echo ""$directory/$oldfilename" ---> "$directory/$newfilename""
+    echo ""$directory/$oldfilename" -> "$directory/$newfilename""
   fi
 done
 exit 0
