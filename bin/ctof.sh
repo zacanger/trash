c332fee352db8f3adcd0d28d977a808b25eea92e
zacanger <zac@zacanger.com>
2020-02-28 16:53:11 -0700

    chore: clean up temp conversion script

--- bin/ctof.sh
+++ bin/ctof.sh
@@ -1,34 +1,36 @@
 #!/usr/bin/env bash
 
-mainmenu () {
-  clear; echo "Temp Conversion Script";echo ""; menu="\t1) \tFahrenheit to Celsius Conversion\n\t2)\tCelsius to Fahrenheit Conversion\n\t0)\tExit";
-  echo -e "$menu";echo "";read -p "Please choose one of the options above : " option
-  while true; do
-     case $option in
-        1)   F2C;
-           ;;
-        2)   C2F;
-           ;;
-        0)   option="";
-           exit;
-           ;;
-        *)   echo "That choice was invalid!!";
-           ;;
-     esac
-  done
+main_menu() {
+  echo '1) Fahrenheit to Celsius'
+  echo '2) Celsius to Fahrenheit'
+  echo ""
+  read -p "Please choose one of the options above: " option
+  case $option in
+    1) F2C;
+      ;;
+    2) C2F
+      ;;
+    *) echo "Invalid choice!"; exit 1
+      ;;
+  esac
 }
 
-F2C () {
-  echo "Fahrenheit to Celsius Conversion";echo "Enter the temperature (in Fahrenheit):";read FT
-  tconv=`echo "($FT - 32) * 5 / 9 " |bc`
-  echo -en "The Temperature in Celsius is ";echo "$tconv" ;read -p "Hit RETURN to continue" temp;mainmenu
+F2C() {
+  echo "Fahrenheit to Celsius Conversion"
+  read -p "Enter the temperature (in Fahrenheit): " FT
+  tconv=$(echo "($FT - 32) * 5 / 9 " | bc)
+  echo -en "The Temperature in Celsius is "
+  echo "$tconv"
+  exit 0
 }
 
-C2F () {
-  echo "Celsius to Fahrenheit Conversion";echo "Enter the temperature (in Celsius):";read CT
-  tconv=`echo "$CT * 9 / 5 + 32 " |bc`
-  echo -en "The Temperature in Fahrenheit is ";echo "$tconv";read -p "Hit RETURN to continue" temp;mainmenu
+C2F() {
+  echo "Celsius to Fahrenheit Conversion"
+  read -p "Enter the temperature (in Celsius): " CT
+  tconv=$(echo "$CT * 9 / 5 + 32 " | bc)
+  echo -en "The Temperature in Fahrenheit is "
+  echo "$tconv"
+  exit 0
 }
 
-mainmenu
-exit
+main_menu
