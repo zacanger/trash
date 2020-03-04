24a73220d24597cb4a4a5f6412ff15ef3a4c38af
zacanger <zac@zacanger.com>
2020-03-03 23:26:18 -0700

    fix: zalarm date parse from args

--- bin/zalarm.sh
+++ bin/zalarm.sh
@@ -3,18 +3,17 @@
 # change the next line to whatever you want
 alarmpath=$HOME/Dropbox/z/.alarm.mp3
 
-if ! hash mplayer 2>/dev/null
-then
+if ! hash mplayer 2>/dev/null; then
   echo 'Please install mplayer!'
   exit 1
 fi
 
-if [[ $# -eq 0 ]] ; then
+if [[ $# -eq 0 ]]; then
   echo "it's now $(date)"
   echo alarm date and time?
   read date
 else
-  date="$1"
+  date="$@"
 fi
 
 echo okay! alarm happening at $(date --date="$date")
