ba10ba83847070aee349d8cfc70227516e5439d1
zacanger <zac@zacanger.com>
2020-04-18 16:36:27 -0600

    chore: rm unused script

--- bin/update-hosts-deny.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# https://github.com/mitchellkrogza/Ultimate.Hosts.Blacklist
-sudo mv /etc/hosts.deny /etc/hosts.deny.bak
-sudo wget https://hosts.ubuntu101.co.za/hosts.deny -O /etc/hosts.deny
