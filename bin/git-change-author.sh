57d16aac940b2fefad881b5c0b90273bd2a87c00
zacanger <zac@zacanger.com>
2020-05-29 14:15:08 -0600

    chore: change author

---
+++ bin/git-change-author.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo 'old email'
+read OLD_EMAIL
+echo 'correct name'
+read CORRECT_NAME
+echo 'correct email'
+read CORRECT_EMAIL
+
+git filter-branch --env-filter '
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+export GIT_COMMITTER_NAME="$CORRECT_NAME"
+export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+export GIT_AUTHOR_NAME="$CORRECT_NAME"
+export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
