3f4eeb259bd1f2f5fe0432a0ffab2bd54d2d6d6e
zacanger <zac@zacanger.com>
2020-06-20 12:37:25 -0600

    chore: cleanup

--- client.js
+++ client.js
@@ -1,8 +1,151 @@
+const handleLoadStop = (_) => {
+  isLoading = false
+}
+
+const handleLoadAbort = (event) => {
+  console.log('Load aborted', event)
+}
+
+const getNextPresetZoom = (zoomFactor) => {
+  const preset = [
+    0.25,
+    0.33,
+    0.5,
+    0.67,
+    0.75,
+    0.9,
+    1,
+    1.1,
+    1.25,
+    1.5,
+    1.75,
+    2,
+    2.5,
+    3,
+    4,
+    5,
+  ]
+
+  let low = 0
+  let high = preset.length - 1
+  let mid
+  while (high - low > 1) {
+    mid = Math.floor((high + low) / 2)
+    if (preset[mid] < zoomFactor) {
+      low = mid
+    } else if (preset[mid] > zoomFactor) {
+      high = mid
+    } else {
+      return { low: preset[mid - 1], high: preset[mid + 1] }
+    }
+  }
+  return { low: preset[low], high: preset[high] }
+}
+
+const increaseZoom = () => {
+  const webview = document.querySelector('webview')
+  webview.getZoom((zoomFactor) => {
+    const nextHigherZoom = getNextPresetZoom(zoomFactor).high
+    webview.setZoom(nextHigherZoom)
+    document.forms['zoom-form']['zoom-text'].value = nextHigherZoom.toString()
+  })
+}
+
+const decreaseZoom = () => {
+  const webview = document.querySelector('webview')
+  webview.getZoom((zoomFactor) => {
+    const nextLowerZoom = getNextPresetZoom(zoomFactor).low
+    webview.setZoom(nextLowerZoom)
+    document.forms['zoom-form']['zoom-text'].value = nextLowerZoom.toString()
+  })
+}
+
+const handleKeyDown = (event) => {
+  if (event.ctrlKey) {
+    switch (event.keyCode) {
+      // Ctrl+.
+      case 107:
+      case 187:
+        event.preventDefault()
+        increaseZoom()
+        break
+
+      // Ctrl-.
+      case 109:
+      case 189:
+        event.preventDefault()
+        decreaseZoom()
+    }
+  }
+}
+
+
+const doLayout = () => {
+  const webview = document.querySelector('webview')
+  const controls = document.querySelector('#controls')
+  const controlsHeight = controls.offsetHeight
+  const windowWidth = document.documentElement.clientWidth
+  const windowHeight = document.documentElement.clientHeight
+  const webviewWidth = windowWidth
+  const webviewHeight = windowHeight - controlsHeight
+
+  webview.style.width = webviewWidth + 'px'
+  webview.style.height = webviewHeight + 'px'
+
+  const sadWebview = document.querySelector('#sad-webview')
+  sadWebview.style.width = webviewWidth + 'px'
+  sadWebview.style.height = webviewHeight * 2 / 3 + 'px'
+  sadWebview.style.paddingTop = webviewHeight / 3 + 'px'
+}
 window.onresize = doLayout
 
-var isLoading = false
+const resetExitedState = () => {
+  document.body.classList.remove('exited')
+  document.body.classList.remove('crashed')
+  document.body.classList.remove('killed')
+}
 
-onload = function () {
+const navigateTo = (url) => {
+  resetExitedState()
+  document.querySelector('webview').src = url
+}
+
+const handleExit = (event) => {
+  document.body.classList.add('exited')
+  if (event.type == 'abnormal') {
+    document.body.classList.add('crashed')
+  } else if (event.type == 'killed') {
+    document.body.classList.add('killed')
+  }
+}
+
+const handleLoadStart = (event) => {
+  document.body.classList.add('loading')
+  isLoading = true
+
+  resetExitedState()
+  if (!event.isTopLevel) {
+    return
+  }
+
+  document.querySelector('#location').value = event.url
+}
+
+const handleLoadRedirect = (event) => {
+  resetExitedState()
+  document.querySelector('#location').value = event.newUrl
+}
+
+const handleLoadCommit = () => {
+  resetExitedState()
+  const webview = document.querySelector('webview')
+  document.querySelector('#location').value = webview.getURL()
+  document.querySelector('#back').disabled = !webview.canGoBack()
+  document.querySelector('#forward').disabled = !webview.canGoForward()
+}
+
+let isLoading = false
+const onload = () => {
   const webview = document.querySelector('webview')
   doLayout()
 
@@ -35,6 +178,7 @@ onload = function () {
     if (!val.contains('://')) {
       val = `http://${val}`
     }
+
     navigateTo(val)
   }
 
@@ -44,302 +188,5 @@ onload = function () {
   webview.addEventListener('did-fail-load', handleLoadAbort)
   webview.addEventListener('did-get-redirect-request', handleLoadRedirect)
   webview.addEventListener('did-finish-load', handleLoadCommit)
-
-  // Test for the presence of the experimental <webview> zoom and find APIs.
-  if (typeof (webview.setZoom) == 'function' && typeof (webview.find) == 'function') {
-    let findMatchCase = false
-
-    document.querySelector('#zoom').onclick = () => {
-      if (document.querySelector('#zoom-box').style.display == '-webkit-flex') {
-        closeZoomBox()
-      } else {
-        openZoomBox()
-      }
-    }
-
-    document.querySelector('#zoom-form').onsubmit = (e) => {
-      e.preventDefault()
-      const zoomText = document.forms['zoom-form']['zoom-text']
-      const zoomFactor = Number(zoomText.value)
-      if (zoomFactor > 5) {
-        zoomText.value = '5'
-        zoomFactor = 5
-      } else if (zoomFactor < 0.25) {
-        zoomText.value = '0.25'
-        zoomFactor = 0.25
-      }
-      webview.setZoom(zoomFactor)
-    }
-
-    document.querySelector('#zoom-in').onclick = (e) => {
-      e.preventDefault()
-      increaseZoom()
-    }
-
-    document.querySelector('#zoom-out').onclick = (e) => {
-      e.preventDefault()
-      decreaseZoom()
-    }
-
-    document.querySelector('#find').onclick = () => {
-      if (document.querySelector('#find-box').style.display == 'block') {
-        document.querySelector('webview').stopFinding()
-        closeFindBox()
-      } else {
-        openFindBox()
-      }
-    }
-
-    document.querySelector('#find-text').oninput = (e) => {
-      webview.find(document.forms['find-form']['find-text'].value, { matchCase: findMatchCase })
-    }
-
-    document.querySelector('#find-text').onkeydown = (e) => {
-      if (event.ctrlKey && event.keyCode == 13) {
-        e.preventDefault()
-        webview.stopFinding('activate')
-        closeFindBox()
-      }
-    }
-
-    document.querySelector('#match-case').onclick = function (e) {
-      e.preventDefault()
-      findMatchCase = !findMatchCase
-      let matchCase = document.querySelector('#match-case')
-      if (findMatchCase) {
-        matchCase.style.color = 'blue'
-        matchCase.style['font-weight'] = 'bold'
-      } else {
-        matchCase.style.color = 'black'
-        matchCase.style['font-weight'] = ''
-      }
-
-      webview.find(document.forms['find-form']['find-text'].value, { matchCase: findMatchCase })
-    }
-
-    document.querySelector('#find-backward').onclick = (e) => {
-      e.preventDefault()
-      webview.find(document.forms['find-form']['find-text'].value, { backward: true, matchCase: findMatchCase })
-    }
-
-    document.querySelector('#find-form').onsubmit = (e) => {
-      e.preventDefault()
-      webview.find(document.forms['find-form']['find-text'].value, { matchCase: findMatchCase })
-    }
-
-    webview.addEventListener('findupdate', handleFindUpdate)
-
-    window.addEventListener('keydown', handleKeyDown)
-  } else {
-    const zoom = document.querySelector('#zoom')
-    const find = document.querySelector('#find')
-    zoom.style.visibility = 'hidden'
-    zoom.style.position = 'absolute'
-    find.style.visibility = 'hidden'
-    find.style.position = 'absolute'
-  }
 }
-
-function navigateTo (url) {
-  resetExitedState()
-  document.querySelector('webview').src = url
-}
-
-function doLayout () {
-  const webview = document.querySelector('webview')
-  const controls = document.querySelector('#controls')
-  const controlsHeight = controls.offsetHeight
-  const windowWidth = document.documentElement.clientWidth
-  const windowHeight = document.documentElement.clientHeight
-  const webviewWidth = windowWidth
-  const webviewHeight = windowHeight - controlsHeight
-
-  webview.style.width = webviewWidth + 'px'
-  webview.style.height = webviewHeight + 'px'
-
-  const sadWebview = document.querySelector('#sad-webview')
-  sadWebview.style.width = webviewWidth + 'px'
-  sadWebview.style.height = webviewHeight * 2 / 3 + 'px'
-  sadWebview.style.paddingTop = webviewHeight / 3 + 'px'
-}
-
-function handleExit (event) {
-  document.body.classList.add('exited')
-  if (event.type == 'abnormal') {
-    document.body.classList.add('crashed')
-  } else if (event.type == 'killed') {
-    document.body.classList.add('killed')
-  }
-}
-
-function resetExitedState () {
-  document.body.classList.remove('exited')
-  document.body.classList.remove('crashed')
-  document.body.classList.remove('killed')
-}
-
-function handleFindUpdate (event) {
-  const findResults = document.querySelector('#find-results')
-  if (event.searchText == '') {
-    findResults.innerText = ''
-  } else {
-    findResults.innerText = event.activeMatchOrdinal + ' of ' + event.numberOfMatches
-  }
-
-  // Ensure that the find box does not obscure the active match.
-  if (event.finalUpdate && !event.canceled) {
-    var findBox = document.querySelector('#find-box')
-    findBox.style.left = ''
-    findBox.style.opacity = ''
-    var findBoxRect = findBox.getBoundingClientRect()
-    if (findBoxObscuresActiveMatch(findBoxRect, event.selectionRect)) {
-      // Move the find box out of the way if there is room on the screen, or
-      // make it semi-transparent otherwise.
-      var potentialLeft = event.selectionRect.left - findBoxRect.width - 10
-      if (potentialLeft >= 5) {
-        findBox.style.left = potentialLeft + 'px'
-      } else {
-        findBox.style.opacity = '0.5'
-      }
-    }
-  }
-}
-
-function findBoxObscuresActiveMatch (findBoxRect, matchRect) {
-  return findBoxRect.left < matchRect.left + matchRect.width &&
-    findBoxRect.right > matchRect.left &&
-    findBoxRect.top < matchRect.top + matchRect.height &&
-    findBoxRect.bottom > matchRect.top
-}
-
-function handleKeyDown (event) {
-  if (event.ctrlKey) {
-    switch (event.keyCode) {
-      // Ctrl+F.
-      case 70:
-        event.preventDefault()
-        openFindBox()
-        break
-
-      // Ctrl++.
-      case 107:
-      case 187:
-        event.preventDefault()
-        increaseZoom()
-        break
-
-      // Ctrl+-.
-      case 109:
-      case 189:
-        event.preventDefault()
-        decreaseZoom()
-    }
-  }
-}
-
-function handleLoadCommit () {
-  resetExitedState()
-  var webview = document.querySelector('webview')
-  document.querySelector('#location').value = webview.getURL()
-  document.querySelector('#back').disabled = !webview.canGoBack()
-  document.querySelector('#forward').disabled = !webview.canGoForward()
-  closeBoxes()
-}
-
-function handleLoadStart (event) {
-  document.body.classList.add('loading')
-  isLoading = true
-
-  resetExitedState()
-  if (!event.isTopLevel) {
-    return
-  }
-
-  document.querySelector('#location').value = event.url
-}
-
-function handleLoadStop (event) {
-  // We don't remove the loading class immediately, instead we let the animation
-  // finish, so that the spinner doesn't jerkily reset back to the 0 position.
-  isLoading = false
-}
-
-function handleLoadAbort (event) {
-  console.log('LoadAbort')
-  console.log('  url: ' + event.url)
-  console.log('  isTopLevel: ' + event.isTopLevel)
-  console.log('  type: ' + event.type)
-}
-
-function handleLoadRedirect (event) {
-  resetExitedState()
-  document.querySelector('#location').value = event.newUrl
-}
-
-function getNextPresetZoom (zoomFactor) {
-  var preset = [0.25, 0.33, 0.5, 0.67, 0.75, 0.9, 1, 1.1, 1.25, 1.5, 1.75, 2,
-    2.5, 3, 4, 5]
-  var low = 0
-  var high = preset.length - 1
-  var mid
-  while (high - low > 1) {
-    mid = Math.floor((high + low) / 2)
-    if (preset[mid] < zoomFactor) {
-      low = mid
-    } else if (preset[mid] > zoomFactor) {
-      high = mid
-    } else {
-      return {low: preset[mid - 1], high: preset[mid + 1]}
-    }
-  }
-  return {low: preset[low], high: preset[high]}
-}
-
-function increaseZoom () {
-  var webview = document.querySelector('webview')
-  webview.getZoom(function (zoomFactor) {
-    var nextHigherZoom = getNextPresetZoom(zoomFactor).high
-    webview.setZoom(nextHigherZoom)
-    document.forms['zoom-form']['zoom-text'].value = nextHigherZoom.toString()
-  })
-}
-
-function decreaseZoom () {
-  var webview = document.querySelector('webview')
-  webview.getZoom(function (zoomFactor) {
-    var nextLowerZoom = getNextPresetZoom(zoomFactor).low
-    webview.setZoom(nextLowerZoom)
-    document.forms['zoom-form']['zoom-text'].value = nextLowerZoom.toString()
-  })
-}
-
-function openZoomBox () {
-  document.querySelector('webview').getZoom(function (zoomFactor) {
-    var zoomText = document.forms['zoom-form']['zoom-text']
-    zoomText.value = Number(zoomFactor.toFixed(6)).toString()
-    document.querySelector('#zoom-box').style.display = '-webkit-flex'
-    zoomText.select()
-  })
-}
-
-function closeZoomBox () {
-  document.querySelector('#zoom-box').style.display = 'none'
-}
-
-function openFindBox () {
-  document.querySelector('#find-box').style.display = 'block'
-  document.forms['find-form']['find-text'].select()
-}
-
-function closeFindBox () {
-  var findBox = document.querySelector('#find-box')
-  findBox.style.display = 'none'
-  findBox.style.left = ''
-  findBox.style.opacity = ''
-  document.querySelector('#find-results').innerText = ''
-}
-
-function closeBoxes () {
-  closeZoomBox()
-  closeFindBox()
-}
+window.onload = onload
