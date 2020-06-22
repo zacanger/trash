c7771426bdda594f4ab01ece3f740761bced8785
zacanger <zac@zacanger.com>
2020-06-21 19:41:42 -0600

    chore: rm monitor stuff

--- bin/screenlayout-thinkpad.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-xrandr --output VGA-1 --mode 1920x1080 --pos 1366x0 --rotate normal --output LVDS-1 --primary --mode 1366x768 --pos 0x312 --rotate normal
-
-feh --bg-fill ~/Dropbox/z/misc/wall.jpg
