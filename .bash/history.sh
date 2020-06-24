0d3359bac14f6c78622687e7f65801ee777e23b0
zacanger <zac@zacanger.com>
2020-06-24 09:46:23 -0600

    chore: more histignore

--- .bash/history.sh
+++ .bash/history.sh
@@ -4,7 +4,7 @@ HISTCONTROL='erasedups:ignoreboth' # ignore lines with spaces, and duplicates
 HISTIGNORE="ls:l:la:lo:lS:lv:a:k:cd:h:history:q:exit:c:clear:erm:clc:cerm"
 HISTIGNORE="$HISTIGNORE:..:...:.:cs:co:ni:ns:vi:reload:gst:edrc:edal:fs:dbst:dbup:dbdn"
 HISTIGNORE="$HISTIGNORE:ncu:gf:gd:g:v:nu:cla:shhh:todo:poweroff:tn:ncdu:startx"
-HISTIGNORE="$HISTIGNORE:ls *:df *:ds *:cd *:g *:a *:f *:k *:v *:vi *:vim *"
+HISTIGNORE="$HISTIGNORE:ls *:df *:ds *:cd *:g *:a *:f *:k *:v *:vi *:vim *:ds *"
 
 if [[ $(uname) == 'Darwin' ]]; then
   HISTSIZE=10000 # length
