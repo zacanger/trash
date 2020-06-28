d98463bb6b9b0ebcbdc0a2e192b61785fc350a5f
zacanger <zac@zacanger.com>
2020-06-28 09:41:51 -0600

    chore: histignore git push

--- .bash/history.sh
+++ .bash/history.sh
@@ -5,7 +5,7 @@ HISTIGNORE="ls:l:la:lo:lS:lv:a:k:cd:h:history:q:exit:c:clear:erm:clc:cerm"
 HISTIGNORE="$HISTIGNORE:..:...:.:cs:co:ni:ns:vi:reload:gst:edrc:edal:fs:ncu"
 HISTIGNORE="$HISTIGNORE:gf:gd:g:v:nu:cla:shhh:todo:poweroff:tn:ncdu:startx"
 HISTIGNORE="$HISTIGNORE:ls *:df *:ds *:cd *:g *:a *:f *:k *:v *:vi *:vim *"
-HISTIGNORE="$HISTIGNORE:ds *:cat *:ag *:grep *:ac *:rbl :"
+HISTIGNORE="$HISTIGNORE:ds *:cat *:ag *:grep *:ac *:rbl *:git push *"
 HISTIGNORE="$HISTIGNORE:rbl:aca:dbup:dbdn:dbst:vv"
 
 if [[ $(uname) == 'Darwin' ]]; then
