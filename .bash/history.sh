e4c698348cd6d96a83011a6ccf2a466480382532
zacanger <zac@zacanger.com>
2020-07-11 20:39:30 -0600

    chore: hist adjustment

--- .bash/history.sh
+++ .bash/history.sh
@@ -4,16 +4,16 @@ HISTCONTROL='erasedups:ignoreboth' # ignore lines with spaces, and duplicates
 HISTIGNORE="ls:l:la:lo:lS:lv:a:k:cd:h:history:q:exit:c:clear:erm:clc:cerm"
 HISTIGNORE="$HISTIGNORE:..:...:.:cs:co:ni:ns:vi:reload:gst:edrc:edal:fs:ncu"
 HISTIGNORE="$HISTIGNORE:gf:gd:g:v:nu:cla:shhh:todo:poweroff:tn:ncdu:startx"
-HISTIGNORE="$HISTIGNORE:ls *:df *:ds *:cd *:g *:a *:f *:k *:v *:vi *:vim *"
-HISTIGNORE="$HISTIGNORE:ds *:cat *:ag *:grep *:ac *:rbl *:git push *"
+HISTIGNORE="$HISTIGNORE:ls *:df *:ds *:cd *:g *:a *:f *:k *"
+HISTIGNORE="$HISTIGNORE:ds *:cat *::ac *:rbl *:git push *"
 HISTIGNORE="$HISTIGNORE:rbl:aca:dbup:dbdn:dbst:vv:sync:gdi:cf:f"
 
 if [[ $(uname) == 'Darwin' ]]; then
-  HISTSIZE=10000 # length
-  HISTFILESIZE=10000 # size
-else
   HISTSIZE=1000 # length
   HISTFILESIZE=1000 # size
+else
+  HISTSIZE=100 # length
+  HISTFILESIZE=100 # size
 fi
 
 HISTTIMEFORMAT='%F %T  ' # timestamp
