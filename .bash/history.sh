490b7c1e28af197c07bf6fca5411b908b950254f
zacanger <zac@zacanger.com>
2020-07-26 12:26:13 -0600

    chore: rm some from histignore

--- .bash/history.sh
+++ .bash/history.sh
@@ -4,7 +4,7 @@ HISTCONTROL='erasedups:ignoreboth' # ignore lines with spaces, and duplicates
 HISTIGNORE="ls:l:la:lo:lS:lv:a:k:cd:h:history:q:exit:c:clear:erm:clc:cerm"
 HISTIGNORE="$HISTIGNORE:..:...:.:cs:co:ni:ns:vi:reload:gst:edrc:edal:fs:ncu"
 HISTIGNORE="$HISTIGNORE:gf:gd:g:v:nu:cla:shhh:todo:poweroff:tn:ncdu:startx"
-HISTIGNORE="$HISTIGNORE:ls *:df *:ds *:cd *:g *:a *:f *:k *"
+HISTIGNORE="$HISTIGNORE:ls *::g *:a *:f *:k *"
 HISTIGNORE="$HISTIGNORE:ds *:cat *::ac *:rbl *:git push *"
 HISTIGNORE="$HISTIGNORE:rbl:aca:dbup:dbdn:dbst:vv:sync:gdi:cf:f"
 
