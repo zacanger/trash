48bd708f8206d3aa82f53074973358c3608bf08d
zacanger <zac@zacanger.com>
2020-08-08 18:50:22 -0600

    chore: hist adjustment

--- .bash/history.sh
+++ .bash/history.sh
@@ -4,15 +4,15 @@ HISTCONTROL='erasedups:ignoreboth' # ignore lines with spaces, and duplicates
 HISTIGNORE="ls:l:la:lo:lS:lv:a:k:cd:h:history:q:exit:c:clear:erm:clc:cerm"
 HISTIGNORE="$HISTIGNORE:..:...:.:cs:co:ni:ns:vi:reload:gst:edrc:edal:fs:ncu"
 HISTIGNORE="$HISTIGNORE:gf:gd:g:v:nu:cla:shhh:todo:poweroff:tn:ncdu:startx"
-HISTIGNORE="$HISTIGNORE:rbl *:git push *:g *"
 HISTIGNORE="$HISTIGNORE:rbl:aca:dbup:dbdn:dbst:vv:sync:gdi:cf:f"
+HISTIGNORE="$HISTIGNORE:rbl *:git push *:g *"
 
 if [[ $(uname) == 'Darwin' ]]; then
   HISTSIZE=10000 # length
   HISTFILESIZE=10000 # size
 else
-  HISTSIZE=100 # length
-  HISTFILESIZE=100 # size
+  HISTSIZE=500 # length
+  HISTFILESIZE=500 # size
 fi
 
 HISTTIMEFORMAT='%F %T  ' # timestamp
