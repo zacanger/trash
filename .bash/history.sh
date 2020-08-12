9bc9a48e5a8320187cffe1e4e6abac7667f26acb
zacanger <zac@zacanger.com>
2020-08-11 21:09:42 -0600

    chore: histignore fix

--- .bash/history.sh
+++ .bash/history.sh
@@ -3,8 +3,8 @@
 HISTCONTROL='erasedups:ignoreboth' # ignore lines with spaces, and duplicates
 HISTIGNORE="ls:l:la:lo:lS:lv:a:k:cd:h:history:q:exit:c:clear:erm:clc:cerm"
 HISTIGNORE="$HISTIGNORE:..:...:.:cs:co:ni:ns:vi:reload:gst:edrc:edal:fs:ncu"
-HISTIGNORE="$HISTIGNORE:gf:gd:g:v:nu:cla:shhh:todo:poweroff:tn:ncdu:startx"
-HISTIGNORE="$HISTIGNORE:rbl:aca:dbup:dbdn:dbst:vv:sync:gdi:cf:f"
+HISTIGNORE="$HISTIGNORE:gd:g:v:nu:cla:shhh:todo:poweroff:tn:ncdu:startx"
+HISTIGNORE="$HISTIGNORE:rbl:aca:dbup:dbdn:dbst:vv:sync:gdi:cf:f:gfa"
 HISTIGNORE="$HISTIGNORE:rbl *:git push *:g *"
 
 if [[ $(uname) == 'Darwin' ]]; then
