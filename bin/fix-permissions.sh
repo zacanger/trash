49464588441da5a1608e527f27b7c2aa8579cd10
zacanger <zac@zacanger.com>
2020-08-13 12:46:02 -0600

    fix: use gfind if on mac

--- bin/fix-permissions.sh
+++ bin/fix-permissions.sh
@@ -47,8 +47,12 @@ _path=""
 _fmode="644"
 _dmode="755"
 _chmod=$(which chmod)
-_find=$(which find)
 _xargs=$(which xargs)
+if hash gfind 2>/dev/null; then
+  _find=$(which gfind)
+else
+  _find=$(which find)
+fi
 
 while getopts ":hvp:f:d:" options; do
   case $options in
