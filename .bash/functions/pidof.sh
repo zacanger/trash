f8bb35167a756e0a74ba76c4ac7b3c4c4a35c996
zacanger <zac@zacanger.com>
2020-09-04 14:06:54 -0600

    fix: pidof fn

--- .bash/functions/pidof.sh
+++ .bash/functions/pidof.sh
@@ -3,9 +3,9 @@
 # because macs don't have pidof
 
 pidof() {
-  if hash pidof 2>/dev/null; then
-    $(which pidof) "$1"
-  else
+  if [[ $(uname) == 'Darwin' ]]; then
     ps -ef | grep -i "$1" | grep -v grep | awk '{print $2}'
+  else
+    $(which pidof) "$1"
   fi
 }
