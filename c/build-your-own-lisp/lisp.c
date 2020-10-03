c4db13cf715af3900cc1f16cf70af7d10ef45eab
zacanger <zac@zacanger.com>
2020-10-03 17:42:28 -0600

    checkpoint

--- c/build-your-own-lisp/lisp.c
+++ c/build-your-own-lisp/lisp.c
@@ -189,53 +189,6 @@ void lval_println(lval* v) {
   putchar('\n');
 }
 
-lval eval_op(lval x, char* op, lval y) {
-  if (x.type == LVAL_ERR) {
-    return x;
-  }
-  if (y.type == LVAL_ERR) {
-    return y;
-  }
-
-  if (strcmp(op, "+") == 0) {
-    return lval_num(x.num + y.num);
-  }
-  if (strcmp(op, "-") == 0) {
-    return lval_num(x.num - y.num);
-  }
-  if (strcmp(op, "*") == 0) {
-    return lval_num(x.num * y.num);
-  }
-  if (strcmp(op, "/") == 0) {
-    if (y.num == 0) {
-      return lval_err(LERR_DIV_ZERO);
-    }
-    return lval_num(x.num / y.num);
-  }
-
-  return lval_err(LERR_BAD_OP);
-}
-
-lval eval(mpc_ast_t* t) {
-  if (strstr(t->tag, "number")) {
-    errno = 0;
-    long x = strtol(t->contents, NULL, 10);
-    return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
-  }
-
-  char* op = t->children[1]->contents;
-
-  lval x = eval(t->children[2]);
-
-  int i = 3;
-  while (strstr(t->children[i]->tag, "expr")) {
-    x = eval_op(x, op, eval(t->children[i]));
-    i++;
-  }
-
-  return x;
-}
-
 lval* lval_pop(lval* v, int i) {
   lval* x = v->cell[i];
   memmove(
