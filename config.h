496f1f67234c7e9635ba7d0600966eb28f0d605a
zacanger <zac@zacanger.com>
2020-10-12 09:49:15 -0600

    chore: better comments

--- config.h
+++ config.h
@@ -3,63 +3,73 @@
 #define CONFIG_H
 
 /* Mod1: alt, Mod4: super */
+/* I prefer using super for everything WM-related since
+ * other programs tend not to use it, and then I don't
+ * have to worry potential conflicts with alt and ctrl
+ * and keybinds in other programs
+ */
 #define MODKEY Mod4Mask
-/* Master, where 1 is 100% */
+
+/* 1 is 100%; master window size */
 #define MASTER_SIZE 0.5
 
-/* Colors (plain old CSS hex codes work here) */
+/* Border colors (plain old CSS hex codes work here) */
 #define FOCUS "rgb:00/FF/FA"
 #define UNFOCUS "rgb:0D/0D/0D"
-
-/* Borders */
 /* Gaps between windows in pixels */
 static int win_gaps = 2;
 /* Width of window borders in pixels */
 static int win_borders = 2;
 
-/* add custom programs here. */
+/* This should be your menu or launcher. It could be
+ * dmenu_run, rofi_run, or some other thing. I launch
+ * my own run script in a terminal.
+ */
 const char *menucmd[] = {"st", "-e", "run.sh", NULL};
 
-/* Use the below to set your terminal of choice */
+/* Your terminal. I use st. Even if you don't change
+ * anything else, you should make sure this is correct,
+ * otherwise you won't be able to do anything
+ */
 const char *termcmd[] = {"st", NULL};
 
-/* Avoid multiple paste */
+/* Used below for super+{1..4} */
 #define DESKTOPCHANGE(K, N) \
   {MODKEY, K, change_desktop, {.i = N}}, \
   {MODKEY | ShiftMask, K, client_to_desktop, {.i = N}}
 
-/* declare stacking mode */
+/* Declare stacking mode */
 extern int swmode;
 
-/* Define Keybinds for Programs Here */
+/* Keybinds */
 static struct key keys[] = {
-    /* modifier(s), key, function, args */
+    /* Modifier(s), Key, Function, Args */
 
-    /* grow and shrink split */
+    /* Grow and shrink split */
     {MODKEY, XK_h, decrease, {NULL}},
     {MODKEY, XK_l, increase, {NULL}},
 
-    /* change focus */
+    /* Change focus */
     {MODKEY, XK_j, next_win, {NULL}},
     {MODKEY, XK_Tab, next_win, {NULL}},
     {MODKEY, XK_k, prev_win, {NULL}},
     {MODKEY | ControlMask, XK_j, focus_prev, {NULL}},
     {MODKEY | ControlMask, XK_k, focus_next, {NULL}},
 
-    /* run things */
+    /* Run things */
     {MODKEY, XK_space, spawn, {.com = menucmd}},
     {MODKEY, XK_Return, spawn, {.com = termcmd}},
 
-    /* change layouts */
+    /* Change layouts */
     {MODKEY | ControlMask, XK_space, switch_mode, {NULL}},
     {MODKEY | ControlMask, XK_Return, swap_master, {NULL}},
     {MODKEY | ControlMask, XK_s, stackmode, {NULL}},
 
-    /* kill app, hard kill everything */
+    /* Kill app, hard kill everything */
     {MODKEY | ControlMask, XK_x, kill_client, {NULL}},
     {MODKEY | ControlMask, XK_q, quit, {NULL}},
 
-    /* change desktop */
+    /* Change desktop */
     DESKTOPCHANGE(XK_1, 0),
     DESKTOPCHANGE(XK_2, 1),
     DESKTOPCHANGE(XK_3, 2),
